ef9b668bd0e9b298a90bc1e5abe77f6e93a5244d
alicjab <alicjab@kainos.com>
2020-05-08 09:34:06 +0200

    refactored sequence manager

--- sequence/sequence_manager.py
+++ sequence/sequence_manager.py
@@ -1,27 +1,29 @@
 from sequence.sequence_factory import get_sequence_generator
 
+TABLE_NAME = 'nhais_outbound_state'
+
 
 class IdGenerator:
     """A component that provides sequential ids"""
 
+    def __init__(self):
+        self.sequence_generator = get_sequence_generator(TABLE_NAME)
+
     async def generate_transaction_id(self) -> int:
         """A function that provides sequential transaction id."""
         key = 'transaction_id'
-        transaction_generator = get_sequence_generator(self.table_name)
-        return await transaction_generator.next(key)
+        return await self.sequence_generator.next(key)
 
     async def generate_interchange_id(self, sender, recipient) -> int:
         """A function that provides sequential interchange id.
         Definition:
         SIS (Send Interchange Sequence) - sequence number for the entire EDIFACT interchange """
         key = f"SIS-{sender}-{recipient}"
-        interchange_generator = get_sequence_generator(self.table_name)
-        return await interchange_generator.next(key)
+        return await self.sequence_generator.next(key)
 
     async def generate_message_id(self, sender, recipient) -> int:
         """A function that provides sequential message id
         Definition:
         SMS (Send Message Sequence) - sequence number applied for each message within an interchange"""
         key = f"SMS-{sender}-{recipient}"
-        message_generator = get_sequence_generator(self.table_name)
-        return await message_generator.next(key)
+        return await self.sequence_generator.next(key)
--- sequence/tests/component_test_ids_generators.py
+++ sequence/tests/component_test_ids_generators.py
@@ -3,6 +3,7 @@
 import unittest
 
 import aioboto3
+from sequence.sequence_factory import get_sequence_generator
 from sequence.sequence_manager import IdGenerator
 from utilities import test_utilities
 from utilities import config
@@ -10,84 +11,68 @@ from utilities import config
 
 class ComponentTestIds(unittest.TestCase):
 
-    def setUp(self):
+    @test_utilities.async_test
+    async def setUp(self):
         config.setup_config("NHAIS")
         self.table_name = 'test_ids_generators_table'
         self.key = 'transaction_id'
         self.endpoint = config.get_config('DYNAMODB_ENDPOINT_URL', None)
         self.region_name = 'eu-west-2'
 
-        # Create instance id generator and replace the default table_name
-        # with test table name used in this test
+        # Create instance id generator and replace the default sequence_generator
+        # with one using the test table
         self.id_generator = IdGenerator()
-        self.id_generator.table_name = self.table_name
+        self.id_generator.sequence_generator = get_sequence_generator(self.table_name)
+
+        async with aioboto3.resource('dynamodb', region_name=self.region_name,
+                                     endpoint_url=self.endpoint) as dynamo_resource:
+            try:
+                table = await dynamo_resource.Table(self.table_name)
+                await table.delete()
+            except dynamo_resource.meta.client.exceptions.ResourceNotFoundException:
+                pass  # table may not exist
+            await self.__create_table(dynamo_resource)
+
+    @test_utilities.async_test
+    async def tearDown(self):
+        async with aioboto3.resource('dynamodb', region_name=self.region_name,
+                                     endpoint_url=self.endpoint) as dynamo_resource:
+            table = await dynamo_resource.Table(self.table_name)
+            await table.delete()
 
     @test_utilities.async_test
     async def test_each_id_has_separate_sequence(self):
-        async with aioboto3.resource('dynamodb', region_name=self.region_name,
-                                     endpoint_url=self.endpoint) as dynamo_resource:
-            try:
-                await self.__create_table(dynamo_resource)
+        # Generate two transaction ids - to make sure that the value increase each time
+        self.assertEqual(await self.id_generator.generate_transaction_id(), 1)
+        self.assertEqual(await self.id_generator.generate_transaction_id(), 2)
 
-                # Generate two transaction ids - to make sure that the value increase each time
-                self.assertEqual(await self.id_generator.generate_transaction_id(), 1)
-                self.assertEqual(await self.id_generator.generate_transaction_id(), 2)
+        # Generate interchange id - the sequence should be independent from that of transaction id,
+        # so it should start from 1
+        self.assertEqual(await self.id_generator.generate_interchange_id(111, 222), 1)
 
-                # Generate interchange id - the sequence should be independent from that of transaction id,
-                # so it should start from 1
-                self.assertEqual(await self.id_generator.generate_interchange_id(111, 222), 1)
-
-                # Generate message id - the sequence should be independent from those of transaction and
-                # interchange ids, so it should start from 1'''
-                self.assertEqual(await self.id_generator.generate_message_id(111, 222), 1)
-
-            except dynamo_resource.meta.client.exceptions.ResourceInUseException:
-                self.fail()
-            finally:
-                table = await dynamo_resource.Table(self.table_name)
-                await table.delete()
+        # Generate message id - the sequence should be independent from those of transaction and
+        # interchange ids, so it should start from 1'''
+        self.assertEqual(await self.id_generator.generate_message_id(111, 222), 1)
 
     @test_utilities.async_test
     async def test_interchange_id_has_separate_sequence_for_each_key(self):
-        async with aioboto3.resource('dynamodb', region_name=self.region_name,
-                                     endpoint_url=self.endpoint) as dynamo_resource:
-            try:
-                await self.__create_table(dynamo_resource)
+        # Generate two interchange ids for key SIS-111-222
+        self.assertEqual(await self.id_generator.generate_interchange_id(111, 222), 1)
+        self.assertEqual(await self.id_generator.generate_interchange_id(111, 222), 2)
 
-                # Generate two interchange ids for key SIS-111-222
-                self.assertEqual(await self.id_generator.generate_interchange_id(111, 222), 1)
-                self.assertEqual(await self.id_generator.generate_interchange_id(111, 222), 2)
-
-                # Generate interchange id for key SIS-AAA-BBB - the sequence should be independent from
-                # that of interchange ids for key SIS-111-222, so it should start from 1'''
-                self.assertEqual(await self.id_generator.generate_interchange_id('AAA', 'BBB'), 1)
-
-            except dynamo_resource.meta.client.exceptions.ResourceInUseException:
-                self.fail()
-            finally:
-                table = await dynamo_resource.Table(self.table_name)
-                await table.delete()
+        # Generate interchange id for key SIS-AAA-BBB - the sequence should be independent from
+        # that of interchange ids for key SIS-111-222, so it should start from 1'''
+        self.assertEqual(await self.id_generator.generate_interchange_id('AAA', 'BBB'), 1)
 
     @test_utilities.async_test
     async def test_message_id_has_separate_sequence_for_each_key(self):
-        async with aioboto3.resource('dynamodb', region_name=self.region_name,
-                                     endpoint_url=self.endpoint) as dynamo_resource:
-            try:
-                await self.__create_table(dynamo_resource)
+        # Generate two message ids for key SIS-111-222
+        self.assertEqual(await self.id_generator.generate_message_id(111, 222), 1)
+        self.assertEqual(await self.id_generator.generate_message_id(111, 222), 2)
 
-                # Generate two message ids for key SIS-111-222
-                self.assertEqual(await self.id_generator.generate_message_id(111, 222), 1)
-                self.assertEqual(await self.id_generator.generate_message_id(111, 222), 2)
-
-                # Generate interchange id for key SIS-AAA-BBB - the sequence should be independent
-                # from that of interchange ids for key SIS-111-222, so it should start from 1
-                self.assertEqual(await self.id_generator.generate_message_id('AAA', 'BBB'), 1)
-
-            except dynamo_resource.meta.client.exceptions.ResourceInUseException:
-                self.fail()
-            finally:
-                table = await dynamo_resource.Table(self.table_name)
-                await table.delete()
+        # Generate interchange id for key SIS-AAA-BBB - the sequence should be independent
+        # from that of interchange ids for key SIS-111-222, so it should start from 1
+        self.assertEqual(await self.id_generator.generate_message_id('AAA', 'BBB'), 1)
 
     async def __create_table(self, dynamo_resource):
         await dynamo_resource.create_table(
