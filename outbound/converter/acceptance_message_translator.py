24c6ab73ca4de64d414aaddfe5c85ff8ee77bb5b
Matt Dooner <mattd@kainos.com>
2020-05-13 08:26:20 +0100

    Fixed: transaction type segment in the correct part of the interchange

---
+++ outbound/converter/acceptance_message_translator.py
@@ -0,0 +1,12 @@
+from fhir.resources.patient import Patient
+
+from edifact.outgoing.models.message import ReferenceTransactionType
+from outbound.converter.base_message_translator import BaseMessageTranslator
+
+
+class AcceptanceMessageTranslator(BaseMessageTranslator):
+    def _append_transaction_type(self):
+        self.segments.append(ReferenceTransactionType(ReferenceTransactionType.TransactionType.ACCEPTANCE))
+
+    def _append_message_body(self, patient: Patient):
+        pass
