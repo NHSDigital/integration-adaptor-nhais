82f5ba93dee7be45243d32c3b1436077d5d9a5a7
alicjab <alicjab@kainos.com>
2020-05-13 09:33:37 +0200

    made sequence manager unit test asynchronous - fix

--- sequence/tests/test_sequence_manager.py
+++ sequence/tests/test_sequence_manager.py
@@ -39,54 +39,54 @@ class TestSequenceManager(unittest.TestCase):
     @async_test
     async def test_generate_interchange_id_for_empty_sender(self, mock_next):
         with self.assertRaises(ValueError):
-            mock_next.return_value = 5
+            mock_next.return_value = awaitable(5)
             await self.id_generator.generate_interchange_id('', 'recipient')
 
     @mock.patch.object(sequence.dynamo_sequence.DynamoSequenceGenerator, 'next')
     @async_test
     async def test_generate_interchange_id_for_none_sender(self, mock_next):
         with self.assertRaises(ValueError):
-            mock_next.return_value = 5
+            mock_next.return_value = awaitable(5)
             await self.id_generator.generate_interchange_id(None, 'recipient')
 
     @mock.patch.object(sequence.dynamo_sequence.DynamoSequenceGenerator, 'next')
     @async_test
     async def test_generate_interchange_id_for_empty_recipient(self, mock_next):
         with self.assertRaises(ValueError):
-            mock_next.return_value = 5
+            mock_next.return_value = awaitable(5)
             await self.id_generator.generate_interchange_id('sender', '')
 
     @mock.patch.object(sequence.dynamo_sequence.DynamoSequenceGenerator, 'next')
     @async_test
     async def test_generate_interchange_id_for_none_recipient(self, mock_next):
         with self.assertRaises(ValueError):
-            mock_next.return_value = 5
+            mock_next.return_value = awaitable(5)
             await self.id_generator.generate_interchange_id('sender', None)
 
     @mock.patch.object(sequence.dynamo_sequence.DynamoSequenceGenerator, 'next')
     @async_test
     async def test_generate_message_id_for_empty_sender(self, mock_next):
         with self.assertRaises(ValueError):
-            mock_next.return_value = 5
+            mock_next.return_value = awaitable(5)
             await self.id_generator.generate_message_id('', 'recipient')
 
     @mock.patch.object(sequence.dynamo_sequence.DynamoSequenceGenerator, 'next')
     @async_test
     async def test_generate_message_id_for_none_sender(self, mock_next):
         with self.assertRaises(ValueError):
-            mock_next.return_value = 5
+            mock_next.return_value = awaitable(5)
             await self.id_generator.generate_message_id(None, 'recipient')
 
     @mock.patch.object(sequence.dynamo_sequence.DynamoSequenceGenerator, 'next')
     @async_test
     async def test_generate_message_id_for_empty_recipient(self, mock_next):
         with self.assertRaises(ValueError):
-            mock_next.return_value = 5
+            mock_next.return_value = awaitable(5)
             await self.id_generator.generate_message_id('sender', '')
 
     @mock.patch.object(sequence.dynamo_sequence.DynamoSequenceGenerator, 'next')
     @async_test
     async def test_generate_message_id_for_none_recipient(self, mock_next):
         with self.assertRaises(ValueError):
-            mock_next.return_value = 5
+            mock_next.return_value = awaitable(5)
             await self.id_generator.generate_message_id('sender', None)
