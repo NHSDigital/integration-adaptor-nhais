b5dd39c5502a644c795175656ebbfb70588027b2
Łukasz Kwaśniewski <l.kwasniewski@kainos.com>
2020-05-21 17:25:31 +0200

    Populate tags to module

--- terraform/aws/modules/module_ecs_service/locals.tf
+++ terraform/aws/modules/module_ecs_service/locals.tf
@@ -1,9 +1,20 @@
 locals {
   resource_prefix = "${var.project}-${var.environment}-${var.module_instance}"
-  default_tags = {
-    Project         = var.project,
-    Environment     = var.environment,
+  default_tags = merge(var.default_tags,{
     Module          = var.module_name,
     Module_Instance = var.module_instance,
-  }
+  })
+
+  # env_vars_list = [
+  #   for e in var.environment_variables : "\{ name = 'e.key' value = e.value \}"
+  # ]
 }
+
+
+/*
+        {
+          name = "MHS_LOG_LEVEL"
+          value = var.mhs_log_level
+        },
+
+        */
