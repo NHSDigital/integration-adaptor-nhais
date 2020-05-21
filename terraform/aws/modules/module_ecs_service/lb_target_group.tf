a66082d49d1125127408155a50001aefad762d80
Łukasz Kwaśniewski <l.kwasniewski@kainos.com>
2020-05-21 17:11:13 +0200

    Remove _ from lb names, shorten the names

--- terraform/aws/modules/module_ecs_service/lb_target_group.tf
+++ terraform/aws/modules/module_ecs_service/lb_target_group.tf
@@ -1,5 +1,5 @@
 resource "aws_lb_target_group" "service_target_group" { 
-  name = "${replace(local.resource_prefix,"_","-")}-lb_tg"
+  name = "${replace(local.resource_prefix,"_","-")}-lb-tg"
   port = var.container_port
   protocol = var.protocol
   vpc_id = var.vpc_id
