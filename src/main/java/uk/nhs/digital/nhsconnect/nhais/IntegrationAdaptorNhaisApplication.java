3f831e83748c0d4fb12a4f4b85288905de43700e
Matt Dooner <mattd@kainos.com>
2020-05-22 16:19:23 +0100

    Added Spring Initializr project

---
+++ src/main/java/uk/nhs/digital/nhsconnect/nhais/IntegrationAdaptorNhaisApplication.java
@@ -0,0 +1,13 @@
+package uk.nhs.digital.nhsconnect.nhais;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntegrationAdaptorNhaisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntegrationAdaptorNhaisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/uk/nhs/digital/nhsconnect/integrationadaptornhais/IntegrationAdaptorNhaisApplicationTests.java
@@ -0,0 +1,13 @@
+package uk.nhs.digital.nhsconnect.integrationadaptornhais;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntegrationAdaptorNhaisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
