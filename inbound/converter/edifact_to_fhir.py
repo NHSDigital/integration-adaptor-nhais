0bd0182c7465a3823bb3704e11020d8c9c323669
alicjab <alicjab@kainos.com>
2020-05-23 01:57:39 +0200

    NIAD-141: work in progress, commented edifact_to_fhir.py

--- inbound/converter/edifact_to_fhir.py
+++ inbound/converter/edifact_to_fhir.py
@@ -9,43 +9,43 @@ class EdifactToFhir:
 
     def convert_edifact_to_fhir(self, edifact: Edifact) -> Patient:
 
-        patient = Patient()
-
-        # ---HEADER ----
-        # 1) UNB -> Interchange Header
-        # get sender and recipient
-        # patient.managingOrganization.identifier.value = sender (of the incoming edifact message)
+        # patient = Patient()
+        #
+        # # ---HEADER ----
+        # # 1) UNB -> Interchange Header
+        # # get sender and recipient
+        # # patient.managingOrganization.identifier.value = sender (of the incoming edifact message)
+        # # patient.generalPractitioner[0].identifier.value = recipient
+        #
+        # interchange_header = edifact.interchange_header
+        # patient.managingOrganization.identifier.value = interchange_header.
         # patient.generalPractitioner[0].identifier.value = recipient
-
-        interchange_header = edifact.interchange_header
-        patient.managingOrganization.identifier.value = interchange_header.
-        patient.generalPractitioner[0].identifier.value = recipient
-
-        # 4) NAD+FHS -> Name and Address
-        # patient.managingOrganization.identifier.value
-        patient.managingOrganization.identifier.value = sender
-        patient.active = None
-        patient.address = None
-        patient.birthDate = None
-        patient.communication = None
-        patient.contact = None
-        patient.deceasedBoolean = None
-        patient.deceasedDateTime = None
-        patient.gender = None
-
-        patient.identifier = None
-        patient.id = None
-        patient.link = None
-
-        patient.maritalStatus = None
-        patient.multipleBirthBoolean = None
-        patient.multipleBirthInteger = None
-        patient.name = None
-        patient.photo = None
-        patient.resource_type = None
-        patient.telecom = None
-
-        return patient
+        #
+        # # 4) NAD+FHS -> Name and Address
+        # # patient.managingOrganization.identifier.value
+        # patient.managingOrganization.identifier.value = sender
+        # patient.active = None
+        # patient.address = None
+        # patient.birthDate = None
+        # patient.communication = None
+        # patient.contact = None
+        # patient.deceasedBoolean = None
+        # patient.deceasedDateTime = None
+        # patient.gender = None
+        #
+        # patient.identifier = None
+        # patient.id = None
+        # patient.link = None
+        #
+        # patient.maritalStatus = None
+        # patient.multipleBirthBoolean = None
+        # patient.multipleBirthInteger = None
+        # patient.name = None
+        # patient.photo = None
+        # patient.resource_type = None
+        # patient.telecom = None
+        #
+        # return patient
 
     # def translate_header(self):
     #     pass
