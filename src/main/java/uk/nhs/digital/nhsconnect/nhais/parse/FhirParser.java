fcbceba4096ad6813fe4d3389f5432670e6e0bea
Peter van der Merwe <p.vandermerwe@kainos.com>
2020-05-23 19:27:34 +0100

    Add missing slf4j annotation

--- src/main/java/uk/nhs/digital/nhsconnect/nhais/parse/FhirParser.java
+++ src/main/java/uk/nhs/digital/nhsconnect/nhais/parse/FhirParser.java
@@ -2,18 +2,20 @@ package uk.nhs.digital.nhsconnect.nhais.parse;
 
 import ca.uhn.fhir.context.FhirContext;
 import ca.uhn.fhir.parser.IParser;
+import lombok.extern.slf4j.Slf4j;
+
 import org.hl7.fhir.r4.model.Parameters;
 import org.hl7.fhir.r4.model.Patient;
 import org.springframework.stereotype.Component;
 
-@Component
+@Component @Slf4j
 public class FhirParser {
     private final FhirContext ctx = FhirContext.forR4();
     private final IParser parser = ctx.newJsonParser();
 
     public Patient parse(String body) {
         //TODO just an example:
-        // LOGGER.info("FhirParser, parsing: " + body);
+        LOGGER.info("FhirParser, parsing: " + body);
         return parser.parseResource(Patient.class, body);
     }
 
