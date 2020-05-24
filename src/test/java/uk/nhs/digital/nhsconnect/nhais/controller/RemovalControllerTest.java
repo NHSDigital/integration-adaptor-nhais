cf5f271b69a7ae9f0df9319804b88ccd009dbc2c
jaroslaww <jaroslaww@kainos.com>
2020-05-24 20:12:49 +0200

    Component tests fixed

--- src/test/java/uk/nhs/digital/nhsconnect/nhais/controller/RemovalControllerTest.java
+++ src/test/java/uk/nhs/digital/nhsconnect/nhais/controller/RemovalControllerTest.java
@@ -6,8 +6,10 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
+import uk.nhs.digital.nhsconnect.nhais.parse.FhirParser;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -21,6 +23,9 @@ public class RemovalControllerTest {
 
     @Autowired
     private ObjectMapper objectMapper;
+
+    @MockBean
+    private FhirParser fhirParser;
 
     @Test
     void whenValidInput_thenReturns202() throws Exception {
