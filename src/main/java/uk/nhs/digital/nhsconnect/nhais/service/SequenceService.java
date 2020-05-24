f698afa9257ca06811e0871bee20465829c29b4e
Matt Dooner <mattd@kainos.com>
2020-05-24 09:42:55 +0100

    Added the start of the unit test for edifact translation

---
+++ src/main/java/uk/nhs/digital/nhsconnect/nhais/service/SequenceService.java
@@ -0,0 +1,17 @@
+package uk.nhs.digital.nhsconnect.nhais.service;
+
+public class SequenceService {
+
+    public String generateTransactionId() {
+        return "1";
+    }
+
+    public String generateInterchangeId(String sender, String recipient) {
+        return "1";
+    }
+
+    public String generateMessageId(String sender, String recipient) {
+        return "1";
+    }
+
+}
