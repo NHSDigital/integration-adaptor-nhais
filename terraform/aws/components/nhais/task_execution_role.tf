df528c18e5f2a5780725591dbb74539643c814a2
Łukasz Kwaśniewski <l.kwasniewski@kainos.com>
2020-05-25 17:20:54 +0200

    Missing "

--- terraform/aws/components/nhais/task_execution_role.tf
+++ terraform/aws/components/nhais/task_execution_role.tf
@@ -3,7 +3,7 @@ resource "aws_iam_role" "ecs_service_task_execution_role" {
   assume_role_policy = data.aws_iam_policy_document.ecs_service_task_execution_policies.json
 }
 
-resource "aws_iam_role_policy_attachment" "ecs_service_task_execution_role_attachment {
+resource "aws_iam_role_policy_attachment" "ecs_service_task_execution_role_attachment" {
   role = aws_iam_role.ecs_service_task_execution_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
