e0c8607ced5cf32d83b9b9abbb4404194b87ff0d
Łukasz Kwaśniewski <l.kwasniewski@kainos.com>
2020-05-25 18:14:43 +0200

    Correct name for instance

--- terraform/aws/components/nhais/docdb_instance.tf
+++ terraform/aws/components/nhais/docdb_instance.tf
@@ -1,12 +1,12 @@
 resource "aws_docdb_cluster_instance" "nhais_db_instance" {
   count = 1
-  identifier = "${local.resource_prefix}-db_instance-${count.index}"
+  identifier = "${replace(local.resource_prefix,"_","-")}-dbinstance-${count.index}"
   cluster_identifier = aws_docdb_cluster.nhais_db_cluster.id
   instance_class = "db.r4.large"
   apply_immediately = true
   availability_zone = local.availability_zones[count.index]
   
   tags = merge(local.default_tags,{
-    Name = "${local.resource_prefix}-db_instance-${count.index}"
+    Name = "${local.resource_prefix}-dbinstance-${count.index}"
   })
 }
