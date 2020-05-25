76c68684008328e6cf2586ee51e79bf19bd33bfd
Łukasz Kwaśniewski <l.kwasniewski@kainos.com>
2020-05-25 19:15:58 +0200

    Set the vpc for DocDB SG

--- terraform/aws/components/nhais/sg_docdb.tf
+++ terraform/aws/components/nhais/sg_docdb.tf
@@ -11,6 +11,7 @@ resource "aws_security_group" "docdb_sg" {
 resource "aws_security_group" "docdb_access_sg" {
   name = "${local.resource_prefix}-docdb_access_sg"
   description = "Security Group that allow access to DocDB in env: ${var.environment}"
+  vpc_id = data.terraform_remote_state.base.outputs.vpc_id
   
   tags = merge(local.default_tags,{
     Name = "${local.resource_prefix}-docdb_access_sg"
