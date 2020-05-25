05dc2737bbbbb8ac7cc373126d7ee5f8982b2125
Łukasz Kwaśniewski <l.kwasniewski@kainos.com>
2020-05-25 19:02:29 +0200

    Only r5 is supported

--- terraform/aws/components/nhais/docdb_instance.tf
+++ terraform/aws/components/nhais/docdb_instance.tf
@@ -2,7 +2,7 @@ resource "aws_docdb_cluster_instance" "nhais_db_instance" {
   count = 1
   identifier = "${replace(local.resource_prefix,"_","-")}-dbinstance-${count.index}"
   cluster_identifier = aws_docdb_cluster.nhais_db_cluster.id
-  instance_class = "db.r4.large"
+  instance_class = "db.r5.large"
   apply_immediately = true
   availability_zone = local.availability_zones[count.index]
   
