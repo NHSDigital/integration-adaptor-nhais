b5d53bc0c56f3bb4af82b8a0cf5fdd16e063788d
Łukasz Kwaśniewski <l.kwasniewski@kainos.com>
2020-05-25 17:51:42 +0200

    Correct some names #3

--- terraform/aws/modules/module_ecs_service/ecs_task_definition.tf
+++ terraform/aws/modules/module_ecs_service/ecs_task_definition.tf
@@ -1,8 +1,8 @@
 resource "aws_ecs_task_definition" "ecs_task_definition" { 
   family = "${local.resource_prefix}-task_definition"
   
-  task_role_arn = data.aws_iam_role.ecs_service_task_role.arn
-  execution_role_arn = data.aws_iam_role.ecs_service_task_execution_role.arn
+  task_role_arn = var.task_role_arn
+  execution_role_arn = var.task_execution_role_arn
   //container_definitions = data.template_file.ecs_task_container_definitions.rendered
   container_definitions = jsonencode(
     [
