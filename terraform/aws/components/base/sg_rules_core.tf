fe6dd429279a85ac075ce1303ce4b3cc78c28a1c
Łukasz Kwaśniewski <l.kwasniewski@kainos.com>
2020-05-26 16:36:16 +0200

    Add NAT GW and route tables for internet access #2

---
+++ terraform/aws/components/base/sg_rules_core.tf
@@ -0,0 +1,8 @@
+resource "aws_security_group_rule" "core_to_internet_443" {
+  type = "egress"
+  from_port = 443
+  to_port = 443
+  protocol = "tcp"
+  security_group_id = aws_security_group.core_sg.id
+  cidr_blocks = ["0.0.0.0/0"]
+}
