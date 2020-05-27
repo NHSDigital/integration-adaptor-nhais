a716f91c7ed3a01e911d6c80861d6ed0237122ea
Łukasz Kwaśniewski <l.kwasniewski@kainos.com>
2020-05-27 13:51:42 +0200

    Define NHAIS ECR repo in account, and use it in nhais component

---
+++ terraform/aws/components/account/variables.tf
@@ -0,0 +1,20 @@
+variable "project" {
+  type = string
+  description = "Name of the project where this code is used"
+}
+
+variable "environment" {
+  type = string
+  description = "Name of the environment"
+}
+
+variable "component" {
+  type = string
+  description = "Name of the component"
+  default = "base"
+}
+
+variable "region" {
+  type = string
+  description = "Region where the resources will be created"
+}
