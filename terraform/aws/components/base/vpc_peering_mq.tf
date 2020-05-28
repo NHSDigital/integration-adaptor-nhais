89bccc7b11dffdd52e11467f5ae7cd85bd5e8c1b
Łukasz Kwaśniewski <l.kwasniewski@kainos.com>
2020-05-28 18:29:58 +0200

    Add dependency on peering

--- terraform/aws/components/base/vpc_peering_mq.tf
+++ terraform/aws/components/base/vpc_peering_mq.tf
@@ -26,13 +26,15 @@ resource "aws_route" "mq_to_base_route" {
   route_table_id = data.aws_vpc.mq_vpc.main_route_table_id
   destination_cidr_block = aws_vpc.base_vpc.cidr_block
   vpc_peering_connection_id = aws_vpc_peering_connection.mq_peering.id
+  depends_on = [aws_vpc_peering_connection.mq_peering]
 }
 
 # Add a route to the supplier VPC in the MHS VPC route table
-resource "aws_route" "mhs_to_supplier_route" {
+resource "aws_route" "base_to_mq_route" {
   route_table_id = aws_route.route_private_to_nat_gw.id
   destination_cidr_block = data.aws_vpc.mq_vpc.cidr_block
   vpc_peering_connection_id = aws_vpc_peering_connection.mq_peering.id
+  depends_on = [aws_vpc_peering_connection.mq_peering]
 }
 
 resource "aws_security_group_rule" "mhs_inbound_security_group_amazon_mq_egress_rule" {
