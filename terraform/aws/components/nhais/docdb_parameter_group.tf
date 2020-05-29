048436f9ff5cc93b3981751177405a8e95c46164
Łukasz Kwaśniewski <l.kwasniewski@kainos.com>
2020-05-29 16:10:12 +0200

    Correct name

--- terraform/aws/components/nhais/docdb_parameter_group.tf
+++ terraform/aws/components/nhais/docdb_parameter_group.tf
@@ -1,16 +1,18 @@
 resource "aws_docdb_cluster_parameter_group" "nhais_db_parameters" {
-  name = "${local.resource_prefix}-db-parameters-3.6"
+  name = "${replace(local.resource_prefix,"_","-")}-db-parameters-3.6"
   family = "docdb3.6"
   description = "Parameter group for MongoDB in env: ${var.environment}"
 
   parameter {
     name = "tls"
     value = "disabled"
+    apply_method = "immediate"
   }
 
   parameter {
     name = "audit_logs"
     value = "enabled"
+    apply_method = "immediate"
   }
 
   tags = merge(local.default_tags,{
