6659f4e782ba0707019424945512fb0098f7778e
Matt Dooner <50910155+mattd-kainos@users.noreply.github.com>
2020-05-29 09:36:59 +0100

    NIAD-119 / NIAD-314: Changed mongodb config to use a connection string to support clusters, replica sets, etc (#49)

--- src/main/java/uk/nhs/digital/nhsconnect/nhais/config/MongoConfig.java
+++ src/main/java/uk/nhs/digital/nhsconnect/nhais/config/MongoConfig.java
@@ -1,37 +1,24 @@
 package uk.nhs.digital.nhsconnect.nhais.config;
 
 
+import com.mongodb.ConnectionString;
 import com.mongodb.MongoClientSettings;
-import com.mongodb.MongoCredential;
-import com.mongodb.ServerAddress;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.config.AbstractMongoClientConfiguration;
-import org.springframework.util.StringUtils;
-
-import static java.util.Collections.singletonList;
 
 @Configuration
 @Slf4j
 public class MongoConfig extends AbstractMongoClientConfiguration {
 
-    @Value("${nhais.mongo.databaseName}")
-    String databaseName;
+    @Value("${spring.data.mongodb.database}")
+    private String databaseName;
 
-    @Value("${nhais.mongo.username}")
-    String username;
+    @Value("${spring.data.mongodb.uri}")
+    private String uri;
 
-    @Value("${nhais.mongo.password}")
-    String password;
-
-    @Value("${nhais.mongo.host}")
-    String host;
-
-    @Value("${nhais.mongo.port}")
-    int port;
-
-    @Value("${nhais.mongo.autoIndexCreation:false}")
+    @Value("${spring.data.mongodb.autoIndexCreation:false}")
     boolean autoIndexCreation;
 
     @Override
@@ -41,14 +28,10 @@ public class MongoConfig extends AbstractMongoClientConfiguration {
 
     @Override
     protected void configureClientSettings(MongoClientSettings.Builder builder) {
-        if(!StringUtils.isEmpty(username) || !StringUtils.isEmpty(password)) {
-            builder.credential(MongoCredential.createCredential(username, databaseName, password.toCharArray()));
-        }
-        builder.applyToClusterSettings(settings -> settings.hosts(singletonList(new ServerAddress(host, port))));
+        builder.applyConnectionString(new ConnectionString(uri));
     }
 
     @Override
-    @Value("${nhais.mongo.autoIndexCreation}")
     protected boolean autoIndexCreation() {
         LOGGER.info("Auto index creation is {}", autoIndexCreation);
         return autoIndexCreation;
