edaef6cb370e29f6d0cd91386b34674b40ed6ba2
Łukasz Kwaśniewski <l.kwasniewski@kainos.com>
2020-05-29 15:13:29 +0200

    valueFrom for secrets

--- terraform/aws/components/nhais/locals_secret_variables.tf
+++ terraform/aws/components/nhais/locals_secret_variables.tf
@@ -2,19 +2,20 @@ locals {
     secret_variables = [
     {
       name = "NHAIS_AMQP_USERNAME"
-      value = data.aws_secretsmanager_secret.mq_username.arn
+      valueFrom = data.aws_secretsmanager_secret.mq_username.arn
     },
     {
       name = "NHAIS_AMQP_PASSWORD"
-      value = data.aws_secretsmanager_secret.mq_password.arn
-    },
-    {
-      name = "NHAIS_MONGO_USERNAME"
-      value = "user"
-    },
-    {
-      name = "NHAIS_MONGO_PASSWORD"
-      value = "pass"
+      valueFrom = data.aws_secretsmanager_secret.mq_password.arn
     }
   ]
 }
+
+    # {
+    #   name = "NHAIS_MONGO_USERNAME"
+    #   value = "user"
+    # },
+    # {
+    #   name = "NHAIS_MONGO_PASSWORD"
+    #   value = "pass"
+    # }
