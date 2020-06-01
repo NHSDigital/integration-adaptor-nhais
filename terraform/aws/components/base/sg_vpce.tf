43a6906751c4cce768b018713cc99ff56268fa7c
Łukasz Kwaśniewski <l.kwasniewski@kainos.com>
2020-06-01 15:18:23 +0200

    descriptions for SGs

--- terraform/aws/components/base/sg_vpce.tf
+++ terraform/aws/components/base/sg_vpce.tf
@@ -1,6 +1,6 @@
 resource "aws_security_group" "cloudwatch_sg" { 
   name = "${local.resource_prefix}-cloudwatch_sg"
-  description = "Security group controlling access to CloudWatch VPC Endpoint"
+  description = "Security group controlling access to CloudWatch VPC Endpoint in env: ${var.environment}"
   vpc_id = aws_vpc.base_vpc.id
 
   tags = merge(local.default_tags, {
@@ -10,7 +10,7 @@ resource "aws_security_group" "cloudwatch_sg" {
 
 resource "aws_security_group" "ecr_sg" {
   name = "${local.resource_prefix}-ecr_sg"
-  description = "Security group controlling access to ECR VPC Endpoint"
+  description = "Security group controlling access to ECR VPC Endpoint in env: ${var.environment}"
   vpc_id = aws_vpc.base_vpc.id
 
   tags = merge(local.default_tags, {
--- terraform/aws/modules/module_ecs_service/security_group.tf
+++ terraform/aws/modules/module_ecs_service/security_group.tf
@@ -4,6 +4,8 @@ resource "aws_security_group" "service_sg" {
   tags = merge(local.default_tags, {
     Name = "${local.resource_prefix}-sg"
   })
+  description = "SG for app: ${var.component} in env: ${var.environment}"
+
 }
 
 resource "aws_security_group" "service_lb_sg" {
@@ -12,4 +14,5 @@ resource "aws_security_group" "service_lb_sg" {
   tags = merge(local.default_tags, {
     Name = "${local.resource_prefix}-lb_sg"
   })
+  description = "SG for Load Balancer for app: ${var.component} in env: ${var.environment}"
 }
