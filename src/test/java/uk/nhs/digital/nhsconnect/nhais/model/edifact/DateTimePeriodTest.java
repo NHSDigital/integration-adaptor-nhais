1f762e02fac7f43dc6d7777e7589bb6f153c12c1
Bartek Sarul <b.sarul@kainos.com>
2020-06-02 15:01:58 +0200

    NIAD-143: Added test cases for GMT and BST

--- src/test/java/uk/nhs/digital/nhsconnect/nhais/model/edifact/DateTimePeriodTest.java
+++ src/test/java/uk/nhs/digital/nhsconnect/nhais/model/edifact/DateTimePeriodTest.java
@@ -2,25 +2,37 @@ package uk.nhs.digital.nhsconnect.nhais.model.edifact;
 
 import org.junit.jupiter.api.Test;
 import uk.nhs.digital.nhsconnect.nhais.exceptions.EdifactValidationException;
-import uk.nhs.digital.nhsconnect.nhais.service.TimestampService;
 
 import java.time.Instant;
+import java.time.ZoneOffset;
 import java.time.ZonedDateTime;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class DateTimePeriodTest {
 
-    private final Instant translationDateTime = ZonedDateTime
-        .of(2020, 4, 28, 20, 58, 0, 0, TimestampService.UKZone)
+    private final Instant TRANSLATION_WINTER_DATE_TIME = ZonedDateTime
+        .of(2020, 3, 28, 20, 58, 0, 0, ZoneOffset.UTC)
+        .toInstant();
+    private final Instant TRANSLATION_SUMMER_DATE_TIME = ZonedDateTime
+        .of(2020, 5, 28, 20, 58, 0, 0, ZoneOffset.UTC)
         .toInstant();
 
     @Test
-    public void testValidDateTimePeriod() throws EdifactValidationException {
-        DateTimePeriod dateTimePeriod = new DateTimePeriod(translationDateTime, DateTimePeriod.TypeAndFormat.TRANSLATION_TIMESTAMP);
+    public void testValidWinterDateTimePeriod() throws EdifactValidationException {
+        DateTimePeriod dateTimePeriod = new DateTimePeriod(TRANSLATION_WINTER_DATE_TIME, DateTimePeriod.TypeAndFormat.TRANSLATION_TIMESTAMP);
 
         String edifact = dateTimePeriod.toEdifact();
 
-        assertThat(edifact).isEqualTo("DTM+137:202004282058:203'");
+        assertThat(edifact).isEqualTo("DTM+137:202003282058:203'");
+    }
+
+    @Test
+    public void testValidSummerDateTimePeriod() throws EdifactValidationException {
+        DateTimePeriod dateTimePeriod = new DateTimePeriod(TRANSLATION_SUMMER_DATE_TIME, DateTimePeriod.TypeAndFormat.TRANSLATION_TIMESTAMP);
+
+        String edifact = dateTimePeriod.toEdifact();
+
+        assertThat(edifact).isEqualTo("DTM+137:202005282158:203'");
     }
 }
--- src/test/java/uk/nhs/digital/nhsconnect/nhais/model/edifact/InterchangeHeaderTest.java
+++ src/test/java/uk/nhs/digital/nhsconnect/nhais/model/edifact/InterchangeHeaderTest.java
@@ -3,9 +3,9 @@ package uk.nhs.digital.nhsconnect.nhais.model.edifact;
 
 import org.junit.jupiter.api.Test;
 import uk.nhs.digital.nhsconnect.nhais.exceptions.EdifactValidationException;
-import uk.nhs.digital.nhsconnect.nhais.service.TimestampService;
 
 import java.time.Instant;
+import java.time.ZoneOffset;
 import java.time.ZonedDateTime;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -13,23 +13,36 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class InterchangeHeaderTest {
 
-    private final Instant translationDateTime = ZonedDateTime
-        .of(2019, 4, 23, 9, 0, 0, 0, TimestampService.UKZone)
+    private final Instant TRANSLATION_WINTER_DATE_TIME = ZonedDateTime
+        .of(2019, 3, 23, 9, 0, 0, 0, ZoneOffset.UTC)
+        .toInstant();
+    private final Instant TRANSLATION_SUMMER_DATE_TIME = ZonedDateTime
+        .of(2019, 5, 23, 9, 0, 0, 0, ZoneOffset.UTC)
         .toInstant();
 
     @Test
-    public void testValidInterchangeHeader() throws EdifactValidationException {
-        InterchangeHeader interchangeHeader = new InterchangeHeader("SNDR", "RECP", translationDateTime);
+    public void testValidInterchangeHeaderWithWinterTime() throws EdifactValidationException {
+        InterchangeHeader interchangeHeader = new InterchangeHeader("SNDR", "RECP", TRANSLATION_WINTER_DATE_TIME);
         interchangeHeader.setSequenceNumber(1L);
 
         String edifact = interchangeHeader.toEdifact();
 
-        assertThat(edifact).isEqualTo("UNB+UNOA:2+SNDR+RECP+190423:0900+00000001'");
+        assertThat(edifact).isEqualTo("UNB+UNOA:2+SNDR+RECP+190323:0900+00000001'");
+    }
+
+    @Test
+    public void testValidInterchangeHeaderWithSummerTime() throws EdifactValidationException {
+        InterchangeHeader interchangeHeader = new InterchangeHeader("SNDR", "RECP", TRANSLATION_SUMMER_DATE_TIME);
+        interchangeHeader.setSequenceNumber(1L);
+
+        String edifact = interchangeHeader.toEdifact();
+
+        assertThat(edifact).isEqualTo("UNB+UNOA:2+SNDR+RECP+190523:1000+00000001'");
     }
 
     @Test
     public void testValidationStateful() {
-        InterchangeHeader interchangeHeader = new InterchangeHeader("SNDR", "RECP", translationDateTime);
+        InterchangeHeader interchangeHeader = new InterchangeHeader("SNDR", "RECP", TRANSLATION_WINTER_DATE_TIME);
 
         assertThatThrownBy(interchangeHeader::validateStateful)
             .isInstanceOf(EdifactValidationException.class)
@@ -38,7 +51,7 @@ public class InterchangeHeaderTest {
 
     @Test
     public void testPreValidationSenderEmptyString() {
-        InterchangeHeader interchangeHeader = new InterchangeHeader("", "RECP", translationDateTime);
+        InterchangeHeader interchangeHeader = new InterchangeHeader("", "RECP", TRANSLATION_WINTER_DATE_TIME);
 
         assertThatThrownBy(interchangeHeader::preValidate)
             .isInstanceOf(EdifactValidationException.class)
@@ -47,7 +60,7 @@ public class InterchangeHeaderTest {
 
     @Test
     public void testPreValidationRecipientEmptyString() {
-        InterchangeHeader interchangeHeader = new InterchangeHeader("SNDR", "", translationDateTime);
+        InterchangeHeader interchangeHeader = new InterchangeHeader("SNDR", "", TRANSLATION_WINTER_DATE_TIME);
 
         assertThatThrownBy(interchangeHeader::preValidate)
             .isInstanceOf(EdifactValidationException.class)
