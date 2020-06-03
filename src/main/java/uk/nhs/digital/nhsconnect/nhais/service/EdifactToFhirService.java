a19f5de4b584ed71fe1c6444c3d801cca12ab4eb
Grzegorz Robak <g.robak@kainos.com>
2020-06-03 15:13:37 +0200

    EDIFACT to FHIR (GP and HA data)

--- src/main/java/uk/nhs/digital/nhsconnect/nhais/service/EdifactToFhirService.java
+++ src/main/java/uk/nhs/digital/nhsconnect/nhais/service/EdifactToFhirService.java
@@ -1,14 +1,53 @@
 package uk.nhs.digital.nhsconnect.nhais.service;
 
-import org.hl7.fhir.r4.model.Parameters;
-import org.springframework.stereotype.Component;
 import uk.nhs.digital.nhsconnect.nhais.model.edifact.Interchange;
+
+import org.hl7.fhir.r4.model.Organization;
+import org.hl7.fhir.r4.model.Parameters;
+import org.hl7.fhir.r4.model.Patient;
+import org.hl7.fhir.r4.model.Practitioner;
+import org.hl7.fhir.r4.model.Reference;
+import org.hl7.fhir.r4.model.Resource;
+import org.springframework.stereotype.Component;
 
 @Component
 public class EdifactToFhirService {
 
-    Parameters convertToFhir(Interchange interchange) {
-        return new Parameters();
+    public Parameters convertToFhir(Interchange interchange) {
+        Patient patient = createPatient("example");
+
+        Reference managingOrganization = createManagingOrganizationReference(interchange);
+        Reference generalPractitioner = createGeneralPractitionerReference(interchange);
+
+        patient.setManagingOrganization(managingOrganization);
+        patient.addGeneralPractitioner(generalPractitioner);
+
+        return createParameters(patient);
     }
 
+    private Parameters createParameters(Resource... resources) {
+        Parameters parameters = new Parameters();
+        for (Resource resource : resources) {
+            parameters.addParameter(new Parameters.ParametersParameterComponent().setResource(resource));
+        }
+        return parameters;
+    }
+
+    private Reference createGeneralPractitionerReference(Interchange interchange) {
+        String gpId = interchange.getGpNameAndAddress().getIdentifier();
+        return new Reference(
+                new Practitioner().setId(gpId)
+            );
+    }
+
+    private Reference createManagingOrganizationReference(Interchange interchange) {
+        String organizationId = interchange.getHealthAuthorityNameAndAddress().getIdentifier();
+        return new Reference(
+                new Organization().setId(organizationId)
+            );
+    }
+
+    private Patient createPatient(String patientId) {
+        return (Patient) new Patient().setId(patientId);
+    }
 }
---
+++ src/test/java/uk/nhs/digital/nhsconnect/nhais/service/EdifactToFhirServiceTest.java
@@ -0,0 +1,63 @@
+package uk.nhs.digital.nhsconnect.nhais.service;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import uk.nhs.digital.nhsconnect.nhais.parse.EdifactParser;
+
+import org.hl7.fhir.r4.model.Parameters;
+import org.hl7.fhir.r4.model.Patient;
+import org.hl7.fhir.r4.model.Resource;
+import org.junit.jupiter.api.Test;
+
+class EdifactToFhirServiceTest {
+
+    private final String exampleMessage = "UNB+UNOA:2+TES5+XX11+020114:1619+00000003'\n" +
+        "UNH+00000004+FHSREG:0:1:FH:FHS001'\n" +
+        "BGM+++507'\n" +
+        "NAD+FHS+XX1:954'\n" +
+        "DTM+137:199201141619:203'\n" +
+        "RFF+950:G1'\n" +
+        "S01+1'\n" +
+        "RFF+TN:18'\n" +
+        "NAD+GP+2750922,295:900'\n" +
+        "NAD+RIC+RT:956'\n" +
+        "QTY+951:6'\n" +
+        "QTY+952:3'\n" +
+        "HEA+ACD+A:ZZZ'\n" +
+        "HEA+ATP+2:ZZZ'\n" +
+        "HEA+BM+S:ZZZ'\n" +
+        "HEA+DM+Y:ZZZ'\n" +
+        "DTM+956:19920114:102'\n" +
+        "LOC+950+GLASGOW'\n" +
+        "FTX+RGI+++BABY AT THE REYNOLDS-THORPE CENTRE'\n" +
+        "S02+2'\n" +
+        "PNA+PAT++++SU:KENNEDY+FO:SARAH+TI:MISS+MI:ANGELA'\n" +
+        "DTM+329:19911209:102'\n" +
+        "PDI+2'\n" +
+        "NAD+PAT++??:26 FARMSIDE CLOSE:ST PAULS CRAY:ORPINGTON:KENT+++++BR6  7ET'\n" +
+        "UNT+24+00000004'\n" +
+        "UNZ+1+00000003'";
+
+    @Test
+    void convertToFhir() {
+
+        Parameters parameters = new EdifactToFhirService().convertToFhir(new EdifactParser().parse(exampleMessage));
+
+        List<Resource> resources = parameters.getParameter().stream()
+            .map(Parameters.ParametersParameterComponent::getResource)
+            .collect(Collectors.toList());
+
+        assertThat(resources).hasOnlyElementsOfType(Patient.class);
+        Patient patient = resources.stream()
+            .filter(resource -> resource instanceof Patient)
+            .map(Patient.class::cast)
+            .findFirst()
+            .orElseThrow();
+
+        assertThat(patient.getManagingOrganization().getResource().getIdElement().getIdPart()).isEqualTo("XX1");
+        assertThat(patient.getGeneralPractitionerFirstRep().getResource().getIdElement().getIdPart()).isEqualTo("2750922,295");
+    }
+}
