0b5aa51506e937fdbe6962ca5e44b1c0eab6ae0d
Matt Dooner <50910155+mattd-kainos@users.noreply.github.com>
2020-06-04 09:16:57 +0100

    NIAD-246 Instructions for using Fake MESH server (#64)
    * Additional instructions for using Fake MESH
    
    * Fix typo
    
    * MAILBOX->MAILBOX_ID

--- mesh/env.example.sh
+++ mesh/env.example.sh
@@ -2,12 +2,12 @@
 # configuration option then comment that line. Don't use empty string '' or you'll likely to break things
 
 # Your MESH mailbox id (sender) can be found in your OpenTest welcome e-mail
-export MAILBOX=''
+export MAILBOX_ID=''
 
-# The password for MAILBOX can be found in your OpenTest welcome e-mail
+# The password for MAILBOX_ID can be found in your OpenTest welcome e-mail
 export MAILBOX_PASSWORD=''
 
-# (optional) mailbox is to send messages to (recipient). If not provided all messages are sent to MAILBOX
+# (optional) mailbox is to send messages to (recipient). If not provided all messages are sent to MAILBOX_ID
 #export TO_MAILBOX=''
 
 # Shared key used to generate auth token. Provided by MESH operator (OpenTest, PTL, etc)
@@ -17,7 +17,10 @@ export SHARED_KEY=''
 #export HOST=''
 
 # (optional) path to the file containing the opentest private key. default: ~/opentest.private.key
-#export OPENTEST_PRIVATE_KEY=''
+#export OPENTEST_ENDPOINT_PRIVATE_KEY=''
 
 # (optional) path the file containg the opentest endpoint certificate. default: ~/opentest.endpoint.cert
 #export OPENTEST_ENDPOINT_CERT=''
+
+# (optional) provide different flags / options for the curl command
+#export CURL_FLAGS="-s -i -k -o /dev/null"
