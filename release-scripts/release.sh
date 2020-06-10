1a030ae5e734bd6a550e080db1d3292892a55ab4
Matt Dooner <mattd@kainos.com>
2020-06-10 17:47:04 +0100

    * release.sh push and tag for docker and git

--- release-scripts/release.sh
+++ release-scripts/release.sh
@@ -14,8 +14,8 @@ docker tag local/nhais:latest nhsdev/nia-nhais-adaptor:${RELEASE_VERSION}
 if [ "$1" == "-y" ];
 then
   echo "Tagging and pushing Docker image and git tag"
-#  docker push nhsdev/nia-nhais-adaptor:${RELEASE_VERSION}
-  git tag -a ${RELEASE_VERSION}
-#  git push origin ${RELEASE_VERSION}
+  docker push nhsdev/nia-nhais-adaptor:${RELEASE_VERSION}
+  git tag -a ${RELEASE_VERSION} -m "Release ${RELEASE_VERSION}"
+  git push origin ${RELEASE_VERSION}
 fi
 
