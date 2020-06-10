3bc2d70e33d047a18179633369d2d0137e04a6f4
Matt Dooner <mattd@kainos.com>
2020-06-10 17:21:13 +0100

    NIAD-338: Release 0.0.1

---
+++ release-scripts/release.sh
@@ -0,0 +1,21 @@
+#!/bin/bash 
+
+set -e
+
+export BUILD_TAG=latest
+export RELEASE_VERSION=0.0.1
+
+cd ..
+
+docker-compose build
+
+docker tag local/nhais:latest nhsdev/nia-nhais-adaptor:${RELEASE_VERSION}
+
+if [ "$1" == "-y" ];
+then
+  echo "Tagging and pushing Docker image and git tag"
+#  docker push nhsdev/nia-nhais-adaptor:${RELEASE_VERSION}
+  git tag -a ${RELEASE_VERSION}
+#  git push origin ${RELEASE_VERSION}
+fi
+
