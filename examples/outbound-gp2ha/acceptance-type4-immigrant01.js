aa22cf36ef04f362717a6771d19b42db0288ab5d
Matt Dooner <mattd@kainos.com>
2020-06-13 10:58:33 +0100

    type 4

---
+++ examples/outbound-gp2ha/acceptance-type4-immigrant01.js
@@ -0,0 +1,76 @@
+{
+  "parameter": [
+    {
+      "name": "gpTradingPartnerCode",
+      "valueString": "TES5"
+    },
+    {
+      "name": "acceptanceCode",
+      "valueString": "A"
+    },
+    {
+      "name": "acceptanceType",
+      "valueString": "4"
+    },
+    {
+      "name": "acceptanceDate",
+      "valueString": "1992-01-15"
+    },
+    {
+      "name": "dateOfUkEntry",
+      "valueString": "1991-08-06"
+    },
+    {
+      "name": "dateOfUkExit",
+      "valueString": "1968-03-05"
+    },
+    {
+      "name": "patient",
+      "resource": {
+        "managingOrganization": {
+          "reference": "XX1"
+        },
+        "generalPractitioner": [
+          {
+            "reference": "2750922,295"
+          }
+        ],
+        "name": [
+          {
+            "family": "HOWES",
+            "given": [
+              "ALISON",
+              "J"
+            ],
+            "prefix": [
+              "MRS"
+            ]
+          }
+        ],
+        "gender": "female",
+        "birthDate": "1965-12-12",
+        "address": [
+          {
+            "line": [
+              "",
+              "13 FOX CRESCENT",
+              "",
+              "BROMLEY",
+              "KENT"
+            ],
+            "postalCode": "BR1  7TQ",
+            "use": "home"
+          }
+        ],
+        "extension": [
+          {
+            "url": "http://hl7.org/fhir/StructureDefinition/patient-birthPlace",
+            "valueString": "LANCASHIRE"
+          }
+        ],
+        "resourceType": "Patient"
+      }
+    }
+  ],
+  "resourceType": "Parameters"
+}
