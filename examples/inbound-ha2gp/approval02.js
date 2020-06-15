7621b34e6fbaca20a1b33b8dc432fdfe98c37473
Matt Dooner <mattd@kainos.com>
2020-06-15 11:24:25 +0100

    fixed approval mistake

--- examples/inbound-ha2gp/approval02.js
+++ examples/inbound-ha2gp/approval02.js
@@ -1,6 +1,6 @@
 var headers = {
     'TransactionType': 'approval',
-    // a rejection is always a reply to a previous outbound Acceptance and needs to include an OperationId matching the
+    // an approval is always a reply to a previous outbound Acceptance and needs to include an OperationId matching the
     // OperationId from the Acceptance that solicited the reply
     // OperationId should be treated as case-insensitive
     'OperationId': '6BA3016176936E006FF0B81D5AFE3AA18B701AA6A51712ED784FFF7678BC32EA'
