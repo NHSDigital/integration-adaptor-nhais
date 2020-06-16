d52dea38c0d8a949b9ce44e8311435c4178d17b1
Matt Dooner <mattd@kainos.com>
2020-06-16 16:35:01 +0100

    amendment fixes

--- examples/inbound-ha2gp/amendment01.js
+++ examples/inbound-ha2gp/amendment01.js
@@ -3,12 +3,13 @@ var headers = {
 }
 
 var body = {
-  "id": "9999999999",
+  "nhsNumber": "AVERT1/1",
   "gpCode": "4826940,281",
-  "gpCipher": "TES5",
-  "haCipher": "XX11",
+  "gpTradingPartnerCode": "TES5",
   "healthcarePartyCode": "XX1",
   "patches": [
+    { "op": "replace", "path": "/name/0/family", "value": "MORRIS" },
+    { "op": "replace", "path": "/name/1/family", "value": "ANDREWS" },
     // if EDIFACT contains a ?? placeholder we'll include it in the FHIR message
     { "op": "replace", "path": "/address/0/line/0", "value": "??"},
     { "op": "replace", "path": "/address/0/line/1", "value": "136 HIGH STREET"},
@@ -16,5 +17,9 @@ var body = {
     { "op": "replace", "path": "/address/0/line/2", "value": ""},
     { "op": "replace", "path": "/address/0/line/3", "value": "BROMLEY"},
     { "op": "replace", "path": "/address/0/line/4", "value": "KENT"},
+    { "op": "replace", "path": "/extension/0", "value": {
+      "url": "https://fhir.nhs.uk/R4/StructureDefinition/Extension-UKCore-NHAIS-DrugsDispensedMarker",
+      "valueBoolean": true},
+    },
   ]
 }
