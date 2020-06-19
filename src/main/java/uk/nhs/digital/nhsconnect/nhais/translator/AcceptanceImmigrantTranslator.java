30b03cad2bbb7d22d3fbdfcc622d4b3a07dbb444
jaroslaww <jaroslaww@kainos.com>
2020-06-19 11:41:55 +0200

    NIAD-381: translator clean-up

--- src/main/java/uk/nhs/digital/nhsconnect/nhais/translator/AcceptanceImmigrantTranslator.java
+++ src/main/java/uk/nhs/digital/nhsconnect/nhais/translator/AcceptanceImmigrantTranslator.java
@@ -22,8 +22,11 @@ import uk.nhs.digital.nhsconnect.nhais.model.edifact.ReferenceTransactionType;
 import uk.nhs.digital.nhsconnect.nhais.model.edifact.Segment;
 import uk.nhs.digital.nhsconnect.nhais.model.edifact.SegmentGroup;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static uk.nhs.digital.nhsconnect.nhais.mapper.FromFhirToEdifactMapper.emptyMapper;
 
 @Component
 @RequiredArgsConstructor(onConstructor = @__(@Autowired))
@@ -41,44 +44,44 @@ public class AcceptanceImmigrantTranslator implements FhirToEdifactTranslator {
 
     @Override
     public List<Segment> translate(Parameters parameters) throws FhirValidationException {
-        List<Segment> segments = new ArrayList<>();
-        //BGM+++507'
-        segments.add(new BeginningOfMessage());
-        //NAD+FHS+XX1:954'
-        segments.add(partyQualifierMapper.map(parameters));
-        //DTM+137:199201151723:203'
-        segments.add(acceptanceDateMapper.map(parameters));
-        //RFF+950:G1'
-        segments.add(new ReferenceTransactionType(ReferenceTransactionType.TransactionType.ACCEPTANCE));
-        //S01+1'
-        segments.add(new SegmentGroup(1));
-        //RFF+TN:20'
-        segments.add(new ReferenceTransactionNumber());
-        //NAD+GP+2750922,295:900'
-        segments.add(gpNameAndAddressMapper.map(parameters));
-        //HEA+ACD+A:ZZZ'
-        segments.add(acceptanceCodeMapper.map(parameters));
-        //HEA+ATP+4:ZZZ'
-        segments.add(acceptanceTypeMapper.map(parameters));
-        //DTM+956:19920115:102' (acceptance date ?)
-        segments.add(new DateTimePeriod(DateTimePeriod.TypeAndFormat.TRANSLATION_TIMESTAMP));
-        //DTM+957:19910806:102'
-        segments.add(personDateOfEntryMapper.map(parameters));
-        //DTM+958:19680305:102' (optional)
-        //date of exit
-        //LOC+950+LANCASHIRE' (optional)
-        //birth place
-        //S02+2'
-        segments.add(new SegmentGroup(2));
-        //PNA+PAT++++SU:HOWES+FO:ALISON+TI:MRS+MI:J'
-        segments.add(personNameMapper.map(parameters));
-        //DTM+329:19651212:102'
-        segments.add(personDateOfBirthMapper.map(parameters));
-        //PDI+2'
-        segments.add(personSexMapper.map(parameters));
-        //NAD+PAT++:13 FOX CRESCENT::BROMLEY:KENT+++++BR1  7TQ'
-        segments.add(personAddressMapper.map(parameters));
-
-        return segments;
+        return Stream.of(
+            //BGM+++507'
+            emptyMapper(new BeginningOfMessage()),
+            //NAD+FHS+XX1:954'
+            partyQualifierMapper,
+            //DTM+137:199201151723:203'
+            acceptanceDateMapper,
+            //RFF+950:G1'
+            emptyMapper(new ReferenceTransactionType(ReferenceTransactionType.TransactionType.ACCEPTANCE)),
+            //S01+1'
+            emptyMapper(new SegmentGroup(1)),
+            //RFF+TN:20'
+            emptyMapper(new ReferenceTransactionNumber()),
+            //NAD+GP+2750922,295:900'
+            gpNameAndAddressMapper,
+            //HEA+ACD+A:ZZZ'
+            acceptanceCodeMapper,
+            //HEA+ATP+4:ZZZ'
+            acceptanceTypeMapper,
+            //DTM+956:19920115:102' (acceptance date ?)
+            emptyMapper(new DateTimePeriod(DateTimePeriod.TypeAndFormat.TRANSLATION_TIMESTAMP)),
+            //DTM+957:19910806:102'
+            personDateOfEntryMapper,
+            //DTM+958:19680305:102' (optional)
+            //date of exit
+            //LOC+950+LANCASHIRE' (optional)
+            //birth place
+            //S02+2'
+            emptyMapper(new SegmentGroup(2)),
+            //PNA+PAT++++SU:HOWES+FO:ALISON+TI:MRS+MI:J'
+            personNameMapper,
+            //DTM+329:19651212:102'
+            personDateOfBirthMapper,
+            //PDI+2'
+            personSexMapper,
+            //NAD+PAT++:13 FOX CRESCENT::BROMLEY:KENT+++++BR1  7TQ'
+            personAddressMapper)
+            .map(mapper -> mapper.map(parameters))
+            .collect(Collectors.toList());
     }
 }
