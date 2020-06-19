4064e7c8cb1f3ed46fce0b148617bd07b360809c
Bartek Sarul <b.sarul@kainos.com>
2020-06-19 09:06:35 +0200

    NIAD-378: ACG-acceptance--gp-to-ha-birth-mandatory (#91)
    * NIAD-378: UAT test data
    
    * NIAD-223: Code refactor
    
    * NIAD-378: Birt type 1 acceptance
    
    * NIAD-378: File rename
    
    * NIAD-378: Cleaned up fhir to only have mandatory fields
    
    * NIAD-378: Updated stub test data
    
    * NIAD-378: Deleted stub test files as we have working ones ready
    
    * NIAD-378: Removed date set as it is set later in code
    
    * NIAD-378: Removed time from date of birth
    
    * NIAD-378: Code review changes

--- src/main/java/uk/nhs/digital/nhsconnect/nhais/mapper/AcceptanceCodeMapper.java
+++ src/main/java/uk/nhs/digital/nhsconnect/nhais/mapper/AcceptanceCodeMapper.java
@@ -1,11 +1,12 @@
 package uk.nhs.digital.nhsconnect.nhais.mapper;
 
 import org.hl7.fhir.r4.model.Parameters;
-
+import org.springframework.stereotype.Component;
 import uk.nhs.digital.nhsconnect.nhais.model.edifact.AcceptanceCode;
-import uk.nhs.digital.nhsconnect.nhais.model.fhir.ParametersExtension;
 import uk.nhs.digital.nhsconnect.nhais.model.fhir.ParameterNames;
+import uk.nhs.digital.nhsconnect.nhais.model.fhir.ParametersExtension;
 
+@Component
 public class AcceptanceCodeMapper implements FromFhirToEdifactMapper<AcceptanceCode> {
 
     public AcceptanceCode map(Parameters parameters) {
