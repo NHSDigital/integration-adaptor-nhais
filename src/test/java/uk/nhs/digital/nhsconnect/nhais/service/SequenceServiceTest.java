19dc2143b7e51de8f203eb6437a4c99b3f85735e
alicjab <alicjab@kainos.com>
2020-06-22 17:29:37 +0200

    NIAD-267, NIAD-332: removed accidental import

--- src/test/java/uk/nhs/digital/nhsconnect/nhais/service/SequenceServiceTest.java
+++ src/test/java/uk/nhs/digital/nhsconnect/nhais/service/SequenceServiceTest.java
@@ -5,7 +5,6 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.testcontainers.shaded.org.bouncycastle.jcajce.provider.symmetric.Serpent;
 
 import uk.nhs.digital.nhsconnect.nhais.exceptions.SenderAndRecipientValidationException;
 import uk.nhs.digital.nhsconnect.nhais.exceptions.SenderValidationException;
