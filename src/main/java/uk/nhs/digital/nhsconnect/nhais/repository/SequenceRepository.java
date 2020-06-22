2d412069fb52a17fe46972878c64de7c02b2a12b
alicjab <alicjab@kainos.com>
2020-06-22 17:17:21 +0200

    NIAD-267, NIAD-332: now transaction id is specific to a sender (GP) and has maximum value of 9_999_999 (instead of 99_999_999)

--- src/main/java/uk/nhs/digital/nhsconnect/nhais/repository/SequenceRepository.java
+++ src/main/java/uk/nhs/digital/nhsconnect/nhais/repository/SequenceRepository.java
@@ -19,24 +19,33 @@ public class SequenceRepository {
     private final static String KEY = "key";
     private final static String SEQUENCE_NUMBER = "sequenceNumber";
     private final static long MAX_SEQUENCE_NUMBER = 100_000_000L;
+    private final static long MAX_TRANSACTION_SEQUENCE_NUMBER = 10_000_000L;
 
     @Autowired
     private MongoOperations mongoOperations;
 
     public Long getNext(String key) {
-        Long seqNumber = increment(key);
+        Long seqNumber = increment(key, MAX_SEQUENCE_NUMBER);
         if (seqNumber == 0) {
-            seqNumber = increment(key);
+            seqNumber = increment(key, MAX_SEQUENCE_NUMBER);
         }
         return seqNumber;
     }
 
-    private Long increment(String key) {
+    public Long getNextForTransaction(String key) {
+        Long seqNumber = increment(key, MAX_TRANSACTION_SEQUENCE_NUMBER);
+        if (seqNumber == 0) {
+            seqNumber = increment(key, MAX_TRANSACTION_SEQUENCE_NUMBER);
+        }
+        return seqNumber;
+    }
+
+    private Long increment(String key, Long maxSequenceNumber) {
         return Objects.requireNonNull(mongoOperations.findAndModify(
                 query(where(KEY).is(key)),
                 new Update().inc(SEQUENCE_NUMBER, 1),
                 options().returnNew(true).upsert(true),
                 OutboundSequenceId.class))
-                .getSequenceNumber() % MAX_SEQUENCE_NUMBER;
+                .getSequenceNumber() % maxSequenceNumber;
     }
 }
