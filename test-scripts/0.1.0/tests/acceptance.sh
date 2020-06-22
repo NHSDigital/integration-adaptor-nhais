d4cfd084e5a73001076f2f621acf72d2ee1e5fe9
Matt Dooner <mattd@kainos.com>
2020-06-22 06:56:01 +0100

    Additional comment for acceptance test

--- test-scripts/0.1.0/tests/acceptance.sh
+++ test-scripts/0.1.0/tests/acceptance.sh
@@ -6,8 +6,7 @@ curl -i --location --request POST 'http://localhost/fhir/Patient/$nhais.acceptan
      --data '@../../../src/intTest/resources/outbound_uat_data/acceptance/type1-birth-mandatory.fhir.json' \
      -H "Content-Type: application/json"
 
-# Doesn't seem that ActiveMQ REST API can delivery contents of AMQP messages
-
+# It seems that the ActiveMQ REST API consumer is unable to read the contents of messages published using AMQP
+# Use the ActiveMQ Admin Console to read the messages instead
 #curl -i --location --request GET 'http://admin:admin@localhost:8161/api/message/nhais_mesh_outbound?type=queue&oneShot=true'
-
 #curl -i --location -XGET 'http://admin:admin@localhost:8161/api/message?destination=queue://nhais_mesh_outbound&oneShot=true'
