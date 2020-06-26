25854faae6af5e8a4ff8c51a4c433fa9971e9ca1
Bartek Sarul <b.sarul@kainos.com>
2020-06-26 13:01:22 +0200

    NIAD-224: Unit tests fixed

--- src/test/java/uk/nhs/digital/nhsconnect/nhais/repository/InboundStateTest.java
+++ src/test/java/uk/nhs/digital/nhsconnect/nhais/repository/InboundStateTest.java
@@ -1,94 +1,98 @@
-//package uk.nhs.digital.nhsconnect.nhais.repository;
-//
-//import org.junit.jupiter.api.BeforeEach;
-//import org.junit.jupiter.api.Test;
-//import org.mockito.Mockito;
-//import uk.nhs.digital.nhsconnect.nhais.model.edifact.DateTimePeriod;
-//import uk.nhs.digital.nhsconnect.nhais.model.edifact.Interchange;
-//import uk.nhs.digital.nhsconnect.nhais.model.edifact.InterchangeHeader;
-//import uk.nhs.digital.nhsconnect.nhais.model.edifact.MessageHeader;
-//import uk.nhs.digital.nhsconnect.nhais.model.edifact.Recep;
-//import uk.nhs.digital.nhsconnect.nhais.model.edifact.ReferenceInterchangeRecep;
-//import uk.nhs.digital.nhsconnect.nhais.model.edifact.ReferenceMessageRecep;
-//import uk.nhs.digital.nhsconnect.nhais.model.edifact.ReferenceTransactionNumber;
-//import uk.nhs.digital.nhsconnect.nhais.model.edifact.ReferenceTransactionType;
-//import uk.nhs.digital.nhsconnect.nhais.model.mesh.WorkflowId;
-//
-//import java.time.Instant;
-//import java.time.ZonedDateTime;
-//import java.util.List;
-//
-//import static org.assertj.core.api.Assertions.assertThat;
-//import static org.mockito.Mockito.when;
-//
-//public class InboundStateTest {
-//
-//    private static final String OPERATION_ID = "4b93239acaf902960fad67a339cfda2c1c0f771d51122627066cfcc667bc6b16";
-//    private static final String SENDER = "some_sender";
-//    private static final String RECIPIENT = "some_recipient";
-//    private static final Instant INTERCHANGE_TIMESTAMP = ZonedDateTime.now().toInstant();
-//    private static final Instant TRANSLATION_TIMESTAMP = INTERCHANGE_TIMESTAMP.plusSeconds(10);
-//    private static final long INTERCHANGE_SEQUENCE = 123L;
-//    private static final long MESSAGE_SEQUENCE = 234L;
-//    private static final long TRANSACTION_NUMBER = 345L;
-//    private static final ReferenceTransactionType.TransactionType TRANSACTION_TYPE = ReferenceTransactionType.TransactionType.ACCEPTANCE;
-//
-//    public static final Interchange INTERCHANGE = Mockito.mock(Interchange.class);
-//    public static final Recep RECEP = Mockito.mock(Recep.class);
-//    public static final InboundState INTERCHANGE_INBOUND_STATE = new InboundState()
-//        .setWorkflowId(WorkflowId.REGISTRATION)
-//        .setOperationId(OPERATION_ID)
-//        .setSender(SENDER)
-//        .setRecipient(RECIPIENT)
-//        .setReceiveInterchangeSequence(INTERCHANGE_SEQUENCE)
-//        .setReceiveMessageSequence(MESSAGE_SEQUENCE)
-//        .setTransactionNumber(TRANSACTION_NUMBER)
-//        .setTransactionType(TRANSACTION_TYPE)
-//        .setTranslationTimestamp(TRANSLATION_TIMESTAMP);
-//    public static final InboundState EXPECTED_RECEP_INBOUND_STATE = new InboundState()
-//        .setWorkflowId(WorkflowId.RECEP)
-//        .setSender(SENDER)
-//        .setRecipient(RECIPIENT)
-//        .setReceiveInterchangeSequence(INTERCHANGE_SEQUENCE)
-//        .setReceiveMessageSequence(MESSAGE_SEQUENCE)
-//        .setTranslationTimestamp(TRANSLATION_TIMESTAMP);
-//
-//    @BeforeEach
-//    void setUp() {
-//        when(InboundStateTest.INTERCHANGE.getInterchangeHeader()).thenReturn(
-//            new InterchangeHeader(SENDER, RECIPIENT, INTERCHANGE_TIMESTAMP).setSequenceNumber(INTERCHANGE_SEQUENCE));
-//        when(InboundStateTest.INTERCHANGE.getMessageHeader()).thenReturn(
-//            new MessageHeader().setSequenceNumber(MESSAGE_SEQUENCE));
-//        when(InboundStateTest.INTERCHANGE.getReferenceTransactionNumber()).thenReturn(
-//            new ReferenceTransactionNumber().setTransactionNumber(TRANSACTION_NUMBER));
-//        when(InboundStateTest.INTERCHANGE.getReferenceTransactionType()).thenReturn(
-//            new ReferenceTransactionType(TRANSACTION_TYPE));
-//        when(InboundStateTest.INTERCHANGE.getTranslationDateTime()).thenReturn(
-//            new DateTimePeriod(TRANSLATION_TIMESTAMP, DateTimePeriod.TypeAndFormat.TRANSLATION_TIMESTAMP));
-//
-//        when(InboundStateTest.RECEP.getInterchangeHeader()).thenReturn(
-//            new InterchangeHeader(SENDER, RECIPIENT, INTERCHANGE_TIMESTAMP).setSequenceNumber(INTERCHANGE_SEQUENCE));
-//        when(InboundStateTest.RECEP.getMessageHeader()).thenReturn(
-//            new MessageHeader().setSequenceNumber(MESSAGE_SEQUENCE));
-//        when(InboundStateTest.RECEP.getDateTimePeriod()).thenReturn(
-//            new DateTimePeriod(TRANSLATION_TIMESTAMP, DateTimePeriod.TypeAndFormat.TRANSLATION_TIMESTAMP));
-//        when(InboundStateTest.RECEP.getReferenceInterchangeRecep()).thenReturn(
-//            new ReferenceInterchangeRecep(54343L, ReferenceInterchangeRecep.RecepCode.RECEIVED, 1));
-//        when(InboundStateTest.RECEP.getReferenceMessageReceps()).thenReturn(
-//            List.of(new ReferenceMessageRecep(456456L, ReferenceMessageRecep.RecepCode.SUCCESS)));
-//    }
-//
-//    @Test
-//    void whenFromInterchangeCalled_thenInboundStateObjectIsCreated() {
-//        var inboundStateFromInterchange = InboundState.fromTransaction(INTERCHANGE);
-//
-//        assertThat(inboundStateFromInterchange).isEqualTo(INTERCHANGE_INBOUND_STATE);
-//    }
-//
-//    @Test
-//    void whenFromRecepCalled_thenInboundStateObjectIsCreated() {
-//        var inboundStateFromInterchange = InboundState.fromRecep(RECEP);
-//
-//        assertThat(inboundStateFromInterchange).isEqualTo(EXPECTED_RECEP_INBOUND_STATE);
-//    }
-//}
+package uk.nhs.digital.nhsconnect.nhais.repository;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import uk.nhs.digital.nhsconnect.nhais.model.edifact.DateTimePeriod;
+import uk.nhs.digital.nhsconnect.nhais.model.edifact.InterchangeHeader;
+import uk.nhs.digital.nhsconnect.nhais.model.edifact.MessageHeader;
+import uk.nhs.digital.nhsconnect.nhais.model.edifact.Recep;
+import uk.nhs.digital.nhsconnect.nhais.model.edifact.ReferenceInterchangeRecep;
+import uk.nhs.digital.nhsconnect.nhais.model.edifact.ReferenceMessageRecep;
+import uk.nhs.digital.nhsconnect.nhais.model.edifact.ReferenceTransactionNumber;
+import uk.nhs.digital.nhsconnect.nhais.model.edifact.ReferenceTransactionType;
+import uk.nhs.digital.nhsconnect.nhais.model.edifact.v2.InterchangeV2;
+import uk.nhs.digital.nhsconnect.nhais.model.edifact.v2.MessageV2;
+import uk.nhs.digital.nhsconnect.nhais.model.edifact.v2.TransactionV2;
+import uk.nhs.digital.nhsconnect.nhais.model.mesh.WorkflowId;
+
+import java.time.Instant;
+import java.time.ZonedDateTime;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
+
+public class InboundStateTest {
+
+    public static final InterchangeV2 INTERCHANGE = Mockito.mock(InterchangeV2.class);
+    public static final MessageV2 MESSAGE = Mockito.mock(MessageV2.class);
+    public static final TransactionV2 TRANSACTION = Mockito.mock(TransactionV2.class);
+    public static final Recep RECEP = Mockito.mock(Recep.class);
+    private static final String OPERATION_ID = "4b93239acaf902960fad67a339cfda2c1c0f771d51122627066cfcc667bc6b16";
+    private static final String SENDER = "some_sender";
+    private static final String RECIPIENT = "some_recipient";
+    private static final Instant INTERCHANGE_TIMESTAMP = ZonedDateTime.now().toInstant();
+    private static final Instant TRANSLATION_TIMESTAMP = INTERCHANGE_TIMESTAMP.plusSeconds(10);
+    private static final long INTERCHANGE_SEQUENCE = 123L;
+    private static final long MESSAGE_SEQUENCE = 234L;
+    public static final InboundState EXPECTED_RECEP_INBOUND_STATE = new InboundState()
+        .setWorkflowId(WorkflowId.RECEP)
+        .setSender(SENDER)
+        .setRecipient(RECIPIENT)
+        .setInterchangeSequence(INTERCHANGE_SEQUENCE)
+        .setMessageSequence(MESSAGE_SEQUENCE)
+        .setTranslationTimestamp(TRANSLATION_TIMESTAMP);
+    private static final long TRANSACTION_NUMBER = 345L;
+    private static final ReferenceTransactionType.TransactionType TRANSACTION_TYPE = ReferenceTransactionType.TransactionType.ACCEPTANCE;
+    public static final InboundState EXPECTED_INTERCHANGE_INBOUND_STATE = new InboundState()
+        .setWorkflowId(WorkflowId.REGISTRATION)
+        .setOperationId(OPERATION_ID)
+        .setSender(SENDER)
+        .setRecipient(RECIPIENT)
+        .setInterchangeSequence(INTERCHANGE_SEQUENCE)
+        .setMessageSequence(MESSAGE_SEQUENCE)
+        .setTransactionNumber(TRANSACTION_NUMBER)
+        .setTransactionType(TRANSACTION_TYPE)
+        .setTranslationTimestamp(TRANSLATION_TIMESTAMP);
+
+    @BeforeEach
+    void setUp() {
+        when(INTERCHANGE.getInterchangeHeader()).thenReturn(new InterchangeHeader(SENDER, RECIPIENT, INTERCHANGE_TIMESTAMP).setSequenceNumber(INTERCHANGE_SEQUENCE));
+        when(INTERCHANGE.getMessages()).thenReturn(List.of(MESSAGE));
+
+        when(MESSAGE.getMessageHeader()).thenReturn(new MessageHeader().setSequenceNumber(MESSAGE_SEQUENCE));
+        when(MESSAGE.getReferenceTransactionType()).thenReturn(new ReferenceTransactionType(TRANSACTION_TYPE));
+        when(MESSAGE.getTranslationDateTime()).thenReturn(new DateTimePeriod(TRANSLATION_TIMESTAMP, DateTimePeriod.TypeAndFormat.TRANSLATION_TIMESTAMP));
+        when(MESSAGE.getInterchange()).thenReturn(INTERCHANGE);
+        when(MESSAGE.getTransactions()).thenReturn(List.of(TRANSACTION));
+
+        when(TRANSACTION.getReferenceTransactionNumber()).thenReturn(new ReferenceTransactionNumber().setTransactionNumber(TRANSACTION_NUMBER));
+        when(TRANSACTION.getMessage()).thenReturn(MESSAGE);
+
+        when(RECEP.getInterchangeHeader()).thenReturn(
+            new InterchangeHeader(SENDER, RECIPIENT, INTERCHANGE_TIMESTAMP).setSequenceNumber(INTERCHANGE_SEQUENCE));
+        when(RECEP.getMessageHeader()).thenReturn(
+            new MessageHeader().setSequenceNumber(MESSAGE_SEQUENCE));
+        when(RECEP.getDateTimePeriod()).thenReturn(
+            new DateTimePeriod(TRANSLATION_TIMESTAMP, DateTimePeriod.TypeAndFormat.TRANSLATION_TIMESTAMP));
+        when(RECEP.getReferenceInterchangeRecep()).thenReturn(
+            new ReferenceInterchangeRecep(54343L, ReferenceInterchangeRecep.RecepCode.RECEIVED, 1));
+        when(RECEP.getReferenceMessageReceps()).thenReturn(
+            List.of(new ReferenceMessageRecep(456456L, ReferenceMessageRecep.RecepCode.SUCCESS)));
+    }
+
+    @Test
+    void whenFromInterchangeCalled_thenInboundStateObjectIsCreated() {
+        var inboundStateFromInterchange = InboundState.fromTransaction(TRANSACTION);
+
+        assertThat(inboundStateFromInterchange).isEqualTo(EXPECTED_INTERCHANGE_INBOUND_STATE);
+    }
+
+    @Test
+    void whenFromRecepCalled_thenInboundStateObjectIsCreated() {
+        var inboundStateFromInterchange = InboundState.fromRecep(RECEP);
+
+        assertThat(inboundStateFromInterchange).isEqualTo(EXPECTED_RECEP_INBOUND_STATE);
+    }
+}
--- src/test/java/uk/nhs/digital/nhsconnect/nhais/service/edifact_to_fhir/RejectionTransactionMapperTest.java
+++ src/test/java/uk/nhs/digital/nhsconnect/nhais/service/edifact_to_fhir/RejectionTransactionMapperTest.java
@@ -1,50 +1,49 @@
-//package uk.nhs.digital.nhsconnect.nhais.service.edifact_to_fhir;
-//
-//import static org.assertj.core.api.Assertions.assertThat;
-//import static org.mockito.Mockito.when;
-//
-//import uk.nhs.digital.nhsconnect.nhais.model.edifact.FreeText;
-//import uk.nhs.digital.nhsconnect.nhais.model.edifact.Interchange;
-//import uk.nhs.digital.nhsconnect.nhais.model.edifact.ReferenceTransactionType;
-//import uk.nhs.digital.nhsconnect.nhais.model.fhir.ParameterNames;
-//import uk.nhs.digital.nhsconnect.nhais.model.fhir.ParametersExtension;
-//
-//import org.assertj.core.api.SoftAssertions;
-//import org.assertj.core.api.junit.jupiter.SoftAssertionsExtension;
-//import org.hl7.fhir.r4.model.Parameters;
-//import org.junit.jupiter.api.Test;
-//import org.junit.jupiter.api.extension.ExtendWith;
-//import org.mockito.Mock;
-//import org.mockito.junit.jupiter.MockitoExtension;
-//
-//@ExtendWith(MockitoExtension.class)
-//@ExtendWith(SoftAssertionsExtension.class)
-//class RejectionTransactionMapperTest {
-//
-//    private static final String TEXT_LITERAL = "some_text_literal";
-//    @Mock
-//    Interchange interchange;
-//    @Mock
-//    FreeText freeText;
-//
-//    @Test
-//    void testMap(SoftAssertions softly) {
-//        when(interchange.getFreeText()).thenReturn(freeText);
-//
-//        when(freeText.getTextLiteral()).thenReturn(TEXT_LITERAL);
-//
-//        var parameters = new Parameters();
-//        new RejectionTransactionMapper().map(parameters, interchange);
-//
-//        ParametersExtension parametersExt = new ParametersExtension(parameters);
-//
-//        softly.assertThat(parameters.getParameter().size()).isEqualTo(1);
-//        softly.assertThat(parametersExt.extractValue(ParameterNames.FREE_TEXT)).isEqualTo(TEXT_LITERAL);
-//    }
-//
-//    @Test
-//    void testGetTransactionType() {
-//        assertThat(new RejectionTransactionMapper().getTransactionType())
-//            .isEqualTo(ReferenceTransactionType.TransactionType.REJECTION);
-//    }
-//}
+package uk.nhs.digital.nhsconnect.nhais.service.edifact_to_fhir;
+
+import org.assertj.core.api.SoftAssertions;
+import org.assertj.core.api.junit.jupiter.SoftAssertionsExtension;
+import org.hl7.fhir.r4.model.Parameters;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import uk.nhs.digital.nhsconnect.nhais.model.edifact.FreeText;
+import uk.nhs.digital.nhsconnect.nhais.model.edifact.ReferenceTransactionType;
+import uk.nhs.digital.nhsconnect.nhais.model.edifact.v2.TransactionV2;
+import uk.nhs.digital.nhsconnect.nhais.model.fhir.ParameterNames;
+import uk.nhs.digital.nhsconnect.nhais.model.fhir.ParametersExtension;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+@ExtendWith(SoftAssertionsExtension.class)
+class RejectionTransactionMapperTest {
+
+    private static final String TEXT_LITERAL = "some_text_literal";
+    @Mock
+    TransactionV2 transaction;
+    @Mock
+    FreeText freeText;
+
+    @Test
+    void testMap(SoftAssertions softly) {
+        when(transaction.getFreeText()).thenReturn(freeText);
+
+        when(freeText.getTextLiteral()).thenReturn(TEXT_LITERAL);
+
+        var parameters = new Parameters();
+        new RejectionTransactionMapper().map(parameters, transaction);
+
+        ParametersExtension parametersExt = new ParametersExtension(parameters);
+
+        softly.assertThat(parameters.getParameter().size()).isEqualTo(1);
+        softly.assertThat(parametersExt.extractValue(ParameterNames.FREE_TEXT)).isEqualTo(TEXT_LITERAL);
+    }
+
+    @Test
+    void testGetTransactionType() {
+        assertThat(new RejectionTransactionMapper().getTransactionType())
+            .isEqualTo(ReferenceTransactionType.TransactionType.REJECTION);
+    }
+}
