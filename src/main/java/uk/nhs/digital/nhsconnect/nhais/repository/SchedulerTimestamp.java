c435fcef0313870031ad7700be3e3c9922e01faa
alicjab <alicjab@kainos.com>
2020-06-30 10:29:30 +0200

    NIAD-349: rename constraint for schedulerTimestamp

--- src/main/java/uk/nhs/digital/nhsconnect/nhais/repository/SchedulerTimestamp.java
+++ src/main/java/uk/nhs/digital/nhsconnect/nhais/repository/SchedulerTimestamp.java
@@ -10,7 +10,7 @@ import org.springframework.data.mongodb.core.mapping.Document;
 
 @CompoundIndexes({
     @CompoundIndex(
-        name = "unique_document1",
+        name = "unique_document",
         def = "{'updateTimestamp' : 1, 'schedulerType': 1}",
         unique = true)
 })
