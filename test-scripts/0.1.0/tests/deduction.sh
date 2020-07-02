6347b39c6d8867b70d8ddf0eaf754e2352164c33
alicjab <alicjab@kainos.com>
2020-07-02 14:37:39 +0200

    NIAD-178: un-ignored uat removal and deduction tests

--- test-scripts/0.1.0/tests/deduction.sh
+++ test-scripts/0.1.0/tests/deduction.sh
@@ -3,5 +3,5 @@
 set -ex
 
 curl -i --location --request POST 'http://localhost/fhir/Patient/$nhais.deduction' \
-     --data '@../../../src/intTest/resources/outbound_uat_data/deduction/app-j-1.fhir.ignore.json' \
+     --data '@../../../src/intTest/resources/outbound_uat_data/deduction/app-j-1.fhir.json' \
      -H "Content-Type: application/json"
--- test-scripts/0.1.0/tests/removal.sh
+++ test-scripts/0.1.0/tests/removal.sh
@@ -3,5 +3,5 @@
 set -ex
 
 curl -i --location --request POST 'http://localhost/fhir/Patient/$nhais.removal' \
-     --data '@../../../src/intTest/resources/outbound_uat_data/removal/app-j-1.fhir.ignore.json' \
+     --data '@../../../src/intTest/resources/outbound_uat_data/removal/app-j-1.fhir.json' \
      -H "Content-Type: application/json"
