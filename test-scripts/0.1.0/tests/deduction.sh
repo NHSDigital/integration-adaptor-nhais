c1ed714358a9d1abfbfc563a2e7c007288127250
alicjab <alicjab@kainos.com>
2020-07-03 14:40:09 +0200

    NIAD-178: removed scripts from tes deployments scripts version 0.1.0

--- test-scripts/0.1.0/tests/deduction.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-set -ex
-
-curl -i --location --request POST 'http://localhost/fhir/Patient/$nhais.deduction' \
-     --data '@../../../src/intTest/resources/outbound_uat_data/deduction/app-j-1.fhir.json' \
-     -H "Content-Type: application/json"
