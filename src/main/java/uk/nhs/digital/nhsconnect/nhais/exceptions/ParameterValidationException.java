ec409bd8738eab0249704adbcb96221da379657d
alicjab <alicjab@kainos.com>
2020-07-03 13:05:30 +0200

    NIAD-178: added operation Outcome to ParameterValidationException

--- src/main/java/uk/nhs/digital/nhsconnect/nhais/exceptions/ParameterValidationException.java
+++ src/main/java/uk/nhs/digital/nhsconnect/nhais/exceptions/ParameterValidationException.java
@@ -1,17 +1,23 @@
 package uk.nhs.digital.nhsconnect.nhais.exceptions;
 
+import uk.nhs.digital.nhsconnect.nhais.utils.OperationOutcomeUtils;
+
 import org.hl7.fhir.instance.model.api.IBaseOperationOutcome;
+import org.hl7.fhir.r4.model.OperationOutcome;
 import org.springframework.http.HttpStatus;
 
 public class ParameterValidationException extends NhaisBaseException implements OperationOutcomeError {
 
+    private final IBaseOperationOutcome operationOutcome;
+
     public ParameterValidationException(String message) {
         super(message);
+        operationOutcome = OperationOutcomeUtils.createFromMessage(message, OperationOutcome.IssueType.UNKNOWN);
     }
 
     @Override
     public IBaseOperationOutcome getOperationOutcome() {
-        return null;
+        return operationOutcome;
     }
 
     @Override
