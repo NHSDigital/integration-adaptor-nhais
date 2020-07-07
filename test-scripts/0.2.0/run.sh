98582d3cd0fe4de471a600f690ae3ae32b2259ac
Matt Dooner <mattd@kainos.com>
2020-07-07 21:25:12 +0100

    Scripts for demo of release

--- test-scripts/0.2.0/run.sh
+++ test-scripts/0.2.0/run.sh
@@ -7,7 +7,7 @@ echo -e "${LIGHT_GREEN}Exporting environment variables${NC}"
 source ../export-env-vars.sh
 
 echo -e "${LIGHT_GREEN}Stopping running containers${NC}" 
-docker-compose -f docker-compose.yml stop; 
+docker-compose -f docker-compose.yml down
 
 echo -e "${LIGHT_GREEN}Build and starting containers${NC}" 
 docker-compose -f docker-compose.yml up -d --no-build
