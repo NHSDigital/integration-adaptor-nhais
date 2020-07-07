f3d956d0b3d8bf1aaec2d4f043156fb2998ed0a2
Grzegorz Robak <g.robak@kainos.com>
2020-07-07 23:37:21 +0200

    NIAD-177: Added handling of outbound amendment invalid requests and integration tests for them

--- src/main/java/uk/nhs/digital/nhsconnect/nhais/controller/AmendmentController.java
+++ src/main/java/uk/nhs/digital/nhsconnect/nhais/controller/AmendmentController.java
@@ -1,22 +1,32 @@
 package uk.nhs.digital.nhsconnect.nhais.controller;
 
 
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.CollectionUtils;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 import uk.nhs.digital.nhsconnect.nhais.exceptions.AmendmentValidationException;
 import uk.nhs.digital.nhsconnect.nhais.model.jsonpatch.AmendmentBody;
+import uk.nhs.digital.nhsconnect.nhais.model.jsonpatch.AmendmentPatch;
 import uk.nhs.digital.nhsconnect.nhais.model.mesh.OutboundMeshMessage;
 import uk.nhs.digital.nhsconnect.nhais.service.JsonPatchToEdifactService;
 import uk.nhs.digital.nhsconnect.nhais.service.OutboundQueueService;
 import uk.nhs.digital.nhsconnect.nhais.utils.HttpHeaders;
 
+import java.io.IOException;
 import java.util.Collections;
+import java.util.stream.Collectors;
 
 @RestController
 @Slf4j
@@ -25,19 +35,62 @@ public class AmendmentController {
 
     private final JsonPatchToEdifactService jsonPatchToEdifactService;
     private final OutboundQueueService outboundQueueService;
+    private final ObjectMapper objectMapper;
 
     @PatchMapping(path= "/fhir/Patient/{nhsNumber}", consumes="application/json", produces="application/json")
     @ResponseStatus(HttpStatus.ACCEPTED)
-    public ResponseEntity<?> amendment(@PathVariable(name= "nhsNumber") String nhsNumber, @RequestBody AmendmentBody amendmentBody) {
+    public ResponseEntity<?> amendment(@PathVariable(name= "nhsNumber") String nhsNumber, @RequestBody String body) {
+        AmendmentBody amendmentBody = parseRequest(body);
         LOGGER.info("Amendment request: {}", amendmentBody);
-        if (!nhsNumber.equals(amendmentBody.getNhsNumber())) {
-            throw new AmendmentValidationException("Request body has different NHS number than provided in request path");
-        }
+        validateRequest(nhsNumber, amendmentBody);
         OutboundMeshMessage meshMessage = jsonPatchToEdifactService.convertToEdifact(amendmentBody);
         outboundQueueService.publish(meshMessage);
         MultiValueMap<String, String> headers = new LinkedMultiValueMap<>();
         headers.put(HttpHeaders.OPERATION_ID, Collections.singletonList(meshMessage.getOperationId()));
         return new ResponseEntity<>(headers, HttpStatus.ACCEPTED);
+    }
+
+    private AmendmentBody parseRequest(@RequestBody String body) {
+        try {
+            JsonParser parser = objectMapper.createParser(body);
+            return parser.readValueAs(AmendmentBody.class);
+        } catch (IOException ex) {
+            throw new AmendmentValidationException(ex.getMessage());
+        }
+    }
+
+    private void validateRequest(String nhsNumber, AmendmentBody amendmentBody) {
+        validateRequiredFields(amendmentBody);
+
+        if (!nhsNumber.equals(amendmentBody.getNhsNumber())) {
+            throw new AmendmentValidationException("Request body has different NHS number than provided in request path");
+        }
+
+        var amendmentPaths = amendmentBody.getPatches().stream()
+                .map(AmendmentPatch::getPath)
+                .collect(Collectors.toSet());
+
+        if (amendmentBody.getPatches().size() != amendmentPaths.size()) {
+            throw new AmendmentValidationException("Request contains path that is used multiple times. Each patch path must only be used once within the amendment request");
+        }
+    }
+
+    private void validateRequiredFields(AmendmentBody amendmentBody) {
+        if (amendmentBody.getNhsNumber() == null) {
+            throw new AmendmentValidationException("NHS number is missing in request body");
+        }
+        if (amendmentBody.getGpCode() == null) {
+            throw new AmendmentValidationException("Existing GP Code is missing in request body");
+        }
+        if (amendmentBody.getHealthcarePartyCode() == null) {
+            throw new AmendmentValidationException("Healthcare authority party code is missing in request body");
+        }
+        if (amendmentBody.getGpTradingPartnerCode() == null) {
+            throw new AmendmentValidationException("GP Trading Partner Code is missing in request body");
+        }
+        if (CollectionUtils.isEmpty(amendmentBody.getPatches())) {
+            throw new AmendmentValidationException("Request body has to contain at least one patch operation");
+        }
     }
 
 }
