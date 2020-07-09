0ab06dbfbb372d05b13e34208d32cd189bbed1c7
Matt Dooner <mattd@kainos.com>
2020-07-09 11:31:39 +0100

    Updated test scripts

--- test-scripts/0.2.0/tests/amendment.sh
+++ test-scripts/0.2.0/tests/amendment.sh
@@ -3,5 +3,5 @@
 set -ex
 
 curl -i --location --request PATCH 'http://localhost/fhir/Patient/9999999999' \
-     --data '@../../../src/intTest/resources/outbound_uat_data/amendment/stub.fhir.json' \
+     --data '@../../../src/intTest/resources/outbound_uat_data/amendment/name-change.fhir.json' \
      -H "Content-Type: application/json"
