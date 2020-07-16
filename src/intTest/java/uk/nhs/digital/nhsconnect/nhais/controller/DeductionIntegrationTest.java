e69b431ebc67bfa3fcf45b5d7a25a5ff0a50cdf7
Bartek Sarul <b.sarul@kainos.com>
2020-07-16 15:11:46 +0200

    NIAD-125: Fix failing UAT test

--- src/intTest/java/uk/nhs/digital/nhsconnect/nhais/controller/DeductionIntegrationTest.java
+++ src/intTest/java/uk/nhs/digital/nhsconnect/nhais/controller/DeductionIntegrationTest.java
@@ -12,6 +12,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.core.io.Resource;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
@@ -31,6 +32,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @SpringBootTest
 @AutoConfigureMockMvc
 @Slf4j
+@DirtiesContext
 public class DeductionIntegrationTest {
     public static final String URL = "/fhir/Patient/$nhais.deduction";
 
