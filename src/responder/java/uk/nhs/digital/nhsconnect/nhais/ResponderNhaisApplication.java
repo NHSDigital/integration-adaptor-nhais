8a6960e0f484014294e8986200f53359c0dee083
Grzegorz Robak <g.robak@kainos.com>
2020-07-24 09:44:19 +0200

    NIAD-284: RECEP responder

---
+++ src/responder/java/uk/nhs/digital/nhsconnect/nhais/ResponderNhaisApplication.java
@@ -0,0 +1,27 @@
+package uk.nhs.digital.nhsconnect.nhais;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jms.annotation.EnableJms;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@EnableJms
+@EnableScheduling
+@SpringBootApplication
+public class ResponderNhaisApplication {
+
+	public static void main(String[] args) {
+	    SpringApplication.run(ResponderNhaisApplication.class, args);
+	}
+
+	@Configuration
+	class Config {
+
+		@Bean
+		String registrationConsumerService() {
+			return RecepResponderService.class.getSimpleName();
+		}
+	}
+}
