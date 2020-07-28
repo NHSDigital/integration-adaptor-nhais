016e2588cc3ec7a8790ef48e386cdf75155e1f0a
Grzegorz Robak <g.robak@kainos.com>
2020-07-28 21:07:30 +0200

    NIAD-284: CR fixes

--- src/main/java/uk/nhs/digital/nhsconnect/nhais/configuration/NhaisMongoClientConfiguration.java
+++ src/main/java/uk/nhs/digital/nhsconnect/nhais/configuration/NhaisMongoClientConfiguration.java
@@ -61,10 +61,10 @@ public class NhaisMongoClientConfiguration extends AbstractMongoClientConfigurat
     private String createConnectionString() {
         LOGGER.info("Creating a connection string for mongo client settings...");
         if (!Strings.isNullOrEmpty(host)) {
-            LOGGER.info("A value was provided from mongodb host ({}). Generating a connection string from individual properties.", host);
+            LOGGER.info("A value was provided from mongodb host. Generating a connection string from individual properties.");
             return createConnectionStringFromProperties();
         } else if (!Strings.isNullOrEmpty(uri)) {
-            LOGGER.info("A mongodb connection string provided in spring.data.mongodb.uri ({}) and will be used to configure the database connection.", uri);
+            LOGGER.info("A mongodb connection string provided in spring.data.mongodb.uri and will be used to configure the database connection.");
             return uri;
         } else {
             LOGGER.error("Mongodb must be configured using a connection string or individual properties. Both uri and host are null or empty");
