3298bc4d267ee1509efe2393436c165555793901
alicjab <alicjab@kainos.com>
2020-08-07 18:13:50 +0200

    NIAD-333: blank surname do not pass validation for all types of acceptance

--- src/main/java/uk/nhs/digital/nhsconnect/nhais/outbound/translator/acceptance/OptionalInputValidator.java
+++ src/main/java/uk/nhs/digital/nhsconnect/nhais/outbound/translator/acceptance/OptionalInputValidator.java
@@ -2,9 +2,12 @@ package uk.nhs.digital.nhsconnect.nhais.outbound.translator.acceptance;
 
 import io.micrometer.core.instrument.util.StringUtils;
 import lombok.RequiredArgsConstructor;
+
 import org.hl7.fhir.r4.model.Parameters;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import uk.nhs.digital.nhsconnect.nhais.model.fhir.ParametersExtension;
 import uk.nhs.digital.nhsconnect.nhais.outbound.mapper.PersonNameMapper;
 import uk.nhs.digital.nhsconnect.nhais.outbound.mapper.PersonPlaceOfBirthMapper;
 
@@ -18,5 +21,9 @@ public class OptionalInputValidator {
 
     public boolean nhsNumberIsMissing(Parameters parameters) {
         return StringUtils.isBlank(new PersonNameMapper().map(parameters).getNhsNumber());
+    }
+
+    public boolean surnameIsMissing(Parameters parameters) {
+        return StringUtils.isBlank(ParametersExtension.extractPatient(parameters).getNameFirstRep().getFamily());
     }
 }
