33a74eebaaccffbf8f2d5725a5ae610a4ab6c97d
Bartek Sarul <b.sarul@kainos.com>
2020-08-19 16:36:16 +0200

    NIAD-184: Custom error handler to always return OperationOutcome FHIR resource (#233)
    Co-authored-by: Alicja Błażejewska <63235583+Alicja-Kainos@users.noreply.github.com>

--- src/main/java/uk/nhs/digital/nhsconnect/nhais/outbound/OperationOutcomeExceptionHandler.java
+++ src/main/java/uk/nhs/digital/nhsconnect/nhais/outbound/OperationOutcomeExceptionHandler.java
@@ -3,6 +3,7 @@ package uk.nhs.digital.nhsconnect.nhais.outbound;
 import lombok.extern.slf4j.Slf4j;
 import org.hl7.fhir.r4.model.OperationOutcome;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.util.LinkedMultiValueMap;
@@ -12,8 +13,8 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
-import uk.nhs.digital.nhsconnect.nhais.rest.exception.OperationOutcomeError;
 import uk.nhs.digital.nhsconnect.nhais.outbound.fhir.FhirParser;
+import uk.nhs.digital.nhsconnect.nhais.rest.exception.OperationOutcomeError;
 import uk.nhs.digital.nhsconnect.nhais.utils.OperationOutcomeUtils;
 
 import static java.util.Collections.singletonList;
@@ -30,7 +31,7 @@ public class OperationOutcomeExceptionHandler extends ResponseEntityExceptionHan
     public final ResponseEntity<String> handleAllErrors(Exception ex, WebRequest request) {
         LOGGER.error("Creating OperationOutcome response for unhandled exception", ex);
         MultiValueMap<String, String> headers = new LinkedMultiValueMap<>();
-        headers.put("content-type", singletonList("application/json"));
+        headers.put(HttpHeaders.CONTENT_TYPE, singletonList("application/json"));
         if (ex instanceof OperationOutcomeError) {
             OperationOutcomeError error = (OperationOutcomeError) ex;
             String content = fhirParser.encodeToString(error.getOperationOutcome());
@@ -39,5 +40,14 @@ public class OperationOutcomeExceptionHandler extends ResponseEntityExceptionHan
         OperationOutcome operationOutcome = OperationOutcomeUtils.createFromMessage(ex.getMessage());
         String content = fhirParser.encodeToString(operationOutcome);
         return new ResponseEntity<>(content, headers, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleExceptionInternal(Exception ex, Object body, HttpHeaders headers, HttpStatus status, WebRequest request) {
+        LOGGER.error("Creating OperationOutcome response for unhandled exception", ex);
+        headers.put(HttpHeaders.CONTENT_TYPE, singletonList("application/json"));
+        OperationOutcome operationOutcome = OperationOutcomeUtils.createFromMessage(ex.getMessage());
+        String content = fhirParser.encodeToString(operationOutcome);
+        return new ResponseEntity<>(content, headers, status);
     }
 }
